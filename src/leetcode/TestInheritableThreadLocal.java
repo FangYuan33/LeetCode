b26b04896a5de7869fa53fe250aaf232954524c3
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2022-11-14 20:17:10 +0800

    写完了InheritableThreadLocal的博客

--- src/leetcode/TestInheritableThreadLocal.java
+++ src/leetcode/TestInheritableThreadLocal.java
@@ -8,22 +8,18 @@ import java.util.concurrent.Executors;
  */
 public class TestInheritableThreadLocal {
 
-    // 取null
-//    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
-
-    private static InheritableThreadLocal<String> threadLocal = new InheritableThreadLocal<>();
+    private static final InheritableThreadLocal<String> threadLocal = new InheritableThreadLocal<>();
 
     // 固定大小的线程池，保证线程复用
-    private static ExecutorService executorService = Executors.newFixedThreadPool(1);
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(1);
 
     public static void main(String[] args) {
-        threadLocal.set("main 线程变量");
-        // 正常取到 main 线程变量
+        threadLocal.set("main线程 变量1");
+        // 正常取到 main线程 变量1
         executorService.execute(() -> System.out.println(threadLocal.get()));
 
-
-        threadLocal.set("main 线程变量2");
-        // 线程复用再取还是 main 线程变量
+        threadLocal.set("main线程 变量2");
+        // 线程复用再取还是 main线程 变量1
         executorService.execute(() -> System.out.println(threadLocal.get()));
     }
 }
