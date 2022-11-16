1f14ab6562c32812c659403897424bfbbfe6a691
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2022-11-16 20:07:40 +0800

    TransmittableThreadLocal解决线程变量线程封闭的问题

--- src/leetcode/TestInheritableThreadLocal.java
+++ src/leetcode/TestInheritableThreadLocal.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
+import com.alibaba.ttl.TtlRunnable;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -8,7 +11,9 @@ import java.util.concurrent.Executors;
  */
 public class TestInheritableThreadLocal {
 
-    private static final InheritableThreadLocal<String> threadLocal = new InheritableThreadLocal<>();
+//    private static final InheritableThreadLocal<String> threadLocal = new InheritableThreadLocal<>();
+
+    private static final TransmittableThreadLocal<String> threadLocal = new TransmittableThreadLocal<>();
 
     // 固定大小的线程池，保证线程复用
     private static final ExecutorService executorService = Executors.newFixedThreadPool(1);
@@ -20,6 +25,10 @@ public class TestInheritableThreadLocal {
 
         threadLocal.set("main线程 变量2");
         // 线程复用再取还是 main线程 变量1
-        executorService.execute(() -> System.out.println(threadLocal.get()));
+//        executorService.execute(() -> System.out.println(threadLocal.get()));
+        // 使用TransmittableThreadLocal解决问题
+        executorService.execute(TtlRunnable.get(() -> System.out.println(threadLocal.get())));
+
+        executorService.shutdown();
     }
 }
