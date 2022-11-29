28327cf6f649ab7c9f5805e152b9408cbbbcf505
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-11-29 09:38:50 +0800

    将Leetcode和工作中的代码测试分包

--- src/testinwork/TestInheritableThreadLocal.java
+++ src/testinwork/TestInheritableThreadLocal.java
@@ -1,4 +1,4 @@
-package leetcode;
+package testinwork;
 
 import com.alibaba.ttl.TransmittableThreadLocal;
 import com.alibaba.ttl.TtlRunnable;
--- src/testinwork/TestSimpleDateFormat.java
+++ src/testinwork/TestSimpleDateFormat.java
@@ -1,4 +1,4 @@
-package leetcode;
+package testinwork;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
