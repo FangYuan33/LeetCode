0bcb1af264dbd8cac6c8b4ac69c1e13e6cae4fd6
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-02-09 15:35:59 +0800

    剑指Offer51: 归并排序

--- src/leetcode/SolutionOffer51.java
+++ src/leetcode/SolutionOffer51.java
@@ -1,19 +1,29 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class SolutionOffer51 {
 
-    // 结果值
-    private int count = 0;
-    // temp数组用于保存有序的数组来更改原来无序的数组
-    private int[] temp;
-
-    public int reversePairs(int[] nums) {
-        temp = new int[nums.length];
-        mergeSort(nums, 0, nums.length - 1);
-
-        return count;
+    public static void main(String[] args) {
+//        System.out.println(new SolutionOffer51().reversePairs(new int[]{4,3,2,1}));
+//        System.out.println(new SolutionOffer51().reversePairs(new int[]{4, 5, 6, 7}));
+//        System.out.println(new SolutionOffer51().reversePairs(new int[]{7, 5, 6, 4}));
+//        System.out.println(new SolutionOffer51().reversePairs(new int[]{233, 2000000001, 234, 2000000006, 235,
+//                2000000003, 236, 2000000007, 237, 2000000002, 2000000005, 233, 233, 233, 233, 233, 2000000004}));
+        System.out.println(new SolutionOffer51().reversePairs(new int[]{7, 10, 8, 12, 9, 7, 11}));
     }
 
+    public int reversePairs(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+
+        return res;
+    }
+
+    private int res = 0;
+
+    /**
+     * 归并排序处理问题
+     */
     private void mergeSort(int[] nums, int left, int right) {
         if (left >= right) {
             return;
@@ -26,35 +36,29 @@ public class SolutionOffer51 {
         merge(nums, left, mid, right);
     }
 
+    /**
+     * 合并，每出现一次左边儿比右边儿大那么就有一个逆序对
+     */
     private void merge(int[] nums, int left, int mid, int right) {
-        // 二分两个数组的起始节点
-        int i = left, j = mid + 1;
-        int t = left;
+        // 辅助数组
+        int[] temp = Arrays.copyOfRange(nums, left, right + 1);
+        // 找出要对比的左右数组在辅助数组中的索引
+        int leftBegin = 0, leftEnd = mid - left;
+        int rightBegin = leftEnd + 1, rightEnd = right - left;
 
-        while (i <= mid && j <= right) {
-            // 左边数组比右边儿小直接存上
-            if (nums[i] <= nums[j]) {
-                temp[t++] = nums[i++];
+        // 注解下边 if 条件的比较条件中，需要把边界条件的比较放在前面！需要把边界条件的比较放在前面！需要把边界条件的比较放在前面！
+        for (int i = left; i <= right; i++) {
+            if (leftBegin > leftEnd) {
+                // 左边数组多且未发生比较的情况
+                nums[i] = temp[rightBegin++];
+            } else if (rightBegin > rightEnd || temp[leftBegin] <= temp[rightBegin]) {
+                // 左数组小或者右数组用完的情况
+                nums[i] = temp[leftBegin++];
             } else {
-                // 大的话直接计算逆序个数
-                // 因为已经将原数组排序了，所以左边当前数大于右边儿数组的数的话
-                // 那么当前数后边儿的数都大于右边儿的这个数，所以这么算
-                count += mid - i + 1;
-                temp[t++] = nums[j++];
+                nums[i] = temp[rightBegin++];
+                // 如果左数组当前数比右侧数大，那么左数组之后的数也比此时这个右侧的数大
+                res += (leftEnd - leftBegin) + 1;
             }
-        }
-
-        // 左右数组数量不等的时候，直接把没比较的放进数组里，后边儿的数是排序后比较大的
-        while (i <= mid) {
-            temp[t++] = nums[i++];
-        }
-        while (j <= right) {
-            temp[t++] = nums[j++];
-        }
-
-        // 将原数组变成有序的，这样才能通过 mid - i + 1 来计算逆序对
-        for (int k = left; k < t; k++) {
-            nums[k] = temp[k];
         }
     }
 }
