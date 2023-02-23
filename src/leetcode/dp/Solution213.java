73b4335caabd29a1a1fc70e384c1e93832df6f54
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-02-23 17:11:38 +0800

    213. 打家劫舍 II

--- src/leetcode/dp/Solution213.java
+++ src/leetcode/dp/Solution213.java
@@ -1,35 +1,37 @@
 package leetcode.dp;
 
-import java.util.Arrays;
 
 public class Solution213 {
-    public static int rob(int[] nums) {
+    public int rob(int[] nums) {
+        // 环问题分成偷第一家和不偷第一家两种偷法
         if (nums.length == 1) {
             return nums[0];
         }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
 
+        return Math.max(doRob(nums, 0, nums.length - 1), doRob(nums, 1, nums.length));
+    }
+
+    private int doRob(int[] nums, int left, int right) {
         int[] dp = new int[nums.length];
 
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for (int i = 1; i < nums.length - 1; i++) {
-            dp[i + 1] = Math.max(dp[i - 1] + nums[i], dp[i]);
-        }
-        int max1 = dp[nums.length - 1];
+        // 初始化前两家最大的偷法
+        dp[left] = nums[left];
+        dp[left + 1] = Math.max(nums[left], nums[left + 1]);
 
-        Arrays.fill(dp, 0);
-        dp[1] = nums[1];
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        for (int i = left + 2; i < right; i++) {
+            // 偷当前家和不偷当前家
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
         }
-        int max2 = dp[nums.length - 1];
 
-        return Math.max(max1, max2);
+        return dp[right - 1];
     }
 
     public static void main(String[] args) {
         int[] arrays = new int[] {2, 3, 2};
 
-        System.out.println(rob(arrays));
+        System.out.println(new Solution213().rob(arrays));
     }
 }
