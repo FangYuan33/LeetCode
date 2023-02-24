2c3076a8cb9e18b7a59331df0e0a6f1ff8feaa35
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-02-24 19:56:53 +0800

    剑指 Offer 07. 重建二叉树

--- src/leetcode/tree/SolutionOffer07.java
+++ src/leetcode/tree/SolutionOffer07.java
@@ -8,32 +8,32 @@ import java.util.List;
 class SolutionOffer07 {
     public static void main(String[] args) {
         TreeNode treeNode = new SolutionOffer07().buildTree(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7});
-
         treeNode.print();
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        ArrayList<Integer> pre = new ArrayList<>(preorder.length);
-        for (int i : preorder) {
-            pre.add(i);
+        List<Integer> preList = new ArrayList<>();
+        for (int num : preorder) {
+            preList.add(num);
         }
-        ArrayList<Integer> in = new ArrayList<>(inorder.length);
-        for (int i : inorder) {
-            in.add(i);
+        List<Integer> inList = new ArrayList<>();
+        for (int num : inorder) {
+            inList.add(num);
         }
 
-        return doBuildTree(pre, in);
+        return doBuildTree(preList, inList);
     }
 
-    private TreeNode doBuildTree(List<Integer> pre, List<Integer> in) {
-        if (pre.isEmpty()) {
+    // 每次都在pre里取值 in只用来定位根节点索引，以此来找右子树
+    private TreeNode doBuildTree(List<Integer> preList, List<Integer> inList) {
+        if (preList.isEmpty()) {
             return null;
         }
 
-        TreeNode node = new TreeNode(pre.get(0));
-        int index = in.indexOf(node.val);
-        node.left = doBuildTree(pre.subList(1, index + 1), in.subList(0, index));
-        node.right = doBuildTree(pre.subList(index + 1, pre.size()), in.subList(index + 1, pre.size()));
+        TreeNode node = new TreeNode(preList.get(0));
+        int rootIndex = inList.indexOf(node.val);
+        node.left = doBuildTree(preList.subList(1, rootIndex + 1), inList.subList(0, rootIndex));
+        node.right = doBuildTree(preList.subList(rootIndex + 1, preList.size()), inList.subList(rootIndex + 1, inList.size()));
 
         return node;
     }
