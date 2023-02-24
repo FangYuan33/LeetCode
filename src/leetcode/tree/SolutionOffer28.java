71481a44ea273e32fdca4117b9fb132f5dba556e
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-02-24 16:14:53 +0800

    剑指 Offer 28. 对称的二叉树

---
+++ src/leetcode/tree/SolutionOffer28.java
@@ -0,0 +1,30 @@
+package leetcode.tree;
+
+import leetcode.TreeNode;
+
+public class SolutionOffer28 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if ((left == null && right != null) || (left != null && right == null)) {
+            return false;
+        }
+        if (left != null && right != null && left.val != right.val) {
+            return false;
+        }
+        if (left == null && right == null) {
+            return true;
+        }
+
+        boolean leftRes = isMirror(left.left, right.right);
+        boolean rightRes = isMirror(left.right, right.left);
+
+        return leftRes && rightRes;
+    }
+}
