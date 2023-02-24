5f52661daf5e13c7cfcbae8a83343e6a5f19bd0e
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-02-24 15:28:18 +0800

    剑指 Offer 55 - II. 平衡二叉树

---
+++ src/leetcode/tree/SolutionOffer552.java
@@ -0,0 +1,28 @@
+package leetcode.tree;
+
+import leetcode.TreeNode;
+
+public class SolutionOffer552 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        int left = midOrder(root.left);
+        int right = midOrder(root.right);
+        boolean res = Math.abs(left - right) <= 1;
+
+        return res && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int midOrder(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = midOrder(root.left);
+        int right = midOrder(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
