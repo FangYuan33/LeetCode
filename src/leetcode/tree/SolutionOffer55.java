1c578758fe9e60af093ca53a5adc77148f180abd
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-02-24 15:17:31 +0800

    剑指 Offer 55 - I. 二叉树的深度

---
+++ src/leetcode/tree/SolutionOffer55.java
@@ -0,0 +1,16 @@
+package leetcode.tree;
+
+import leetcode.TreeNode;
+
+public class SolutionOffer55 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
