b53192d14eb5efb6f26150059fd6bcdefe7f318f
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2023-02-25 15:51:45 +0800

    25. K 个一组翻转链表

---
+++ src/leetcode/linkedlist/Solution25.java
@@ -0,0 +1,55 @@
+package leetcode.linkedlist;
+
+import leetcode.ListNode;
+
+public class Solution25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // 整个链表的前驱节点，用作结果返回
+        ListNode pre = new ListNode(-1);
+        pre.next = head;
+
+        // 反转链表前的前驱节点
+        ListNode reversePre = pre;
+        // 每次链表反转的尾节点
+        ListNode end = pre;
+        while (end != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            // 说明不用再反转了
+            if (end == null) {
+                break;
+            }
+
+            // 开始反转链表的节点
+            ListNode begin = reversePre.next;
+            // 下一组开始的节点
+            ListNode nextBegin = end.next;
+            // 拆分出反转链表
+            end.next = null;
+            // 开始反转
+            reversePre.next = reverse(begin);
+            // 反转完毕后组装，begin节点变成了尾巴节点
+            begin.next = nextBegin;
+
+            // 重新开始下一组
+            reversePre = begin;
+            end = begin;
+        }
+
+        return pre.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+
+        return pre;
+    }
+}
--- src/leetcode/linkedlist/Solution92.java
+++ src/leetcode/linkedlist/Solution92.java
@@ -6,17 +6,16 @@ public class Solution92 {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode res = new ListNode(-1);
         res.next = head;
-        int flag = left;
 
         // 找到反转前置节点
-        ListNode pre = new ListNode(-1);
+        ListNode pre = res;
         pre.next = head;
         while (left != 1) {
             left--;
             pre = pre.next;
         }
         // 找到前置尾巴节点，避免空指针异常
-        ListNode preTail = new ListNode(-1);
+        ListNode preTail = res;
         preTail.next = head;
         while (right != 0) {
             right--;
@@ -25,23 +24,18 @@ public class Solution92 {
 
         // 断开分割出中间要反转的链表
         ListNode begin = pre.next;
-        pre.next = null;
         ListNode tail = preTail.next;
         preTail.next = null;
-        // 反转链表
-        ListNode newList = reverse(begin, tail);
-        // 头结点指向反转后的链表
-        pre.next = newList;
+        // 反转链表，头结点指向反转后的链表
+        pre.next = reverse(begin);
+        // 组装链表，begin变成了反转后的尾巴节点
+        begin.next = tail;
 
-        // 如果是从头反转，返回pre
-        if (flag == 1) {
-            return pre.next;
-        }
         return res.next;
     }
 
-    private ListNode reverse(ListNode head, ListNode tail) {
-        ListNode pre = tail;
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
 
         while (head != null) {
             ListNode temp = head.next;
