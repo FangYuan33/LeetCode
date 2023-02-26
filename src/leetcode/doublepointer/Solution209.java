5443baa4c4fa545b855ad4a7c4c9e73feda725ee
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2023-02-26 10:14:17 +0800

    209. 长度最小的子数组

---
+++ src/leetcode/doublepointer/Solution209.java
@@ -0,0 +1,29 @@
+package leetcode.doublepointer;
+
+public class Solution209 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int res = Integer.MAX_VALUE;
+
+        // 初始窗口
+        int left = 0, right = 0;
+        int sum = nums[right];
+
+        while (right < nums.length) {
+            if (sum < target) {
+                // 窗户变大
+                right++;
+                if (right < nums.length) {
+                    sum += nums[right];
+                }
+            } else {
+                res = Math.min(res, right - left + 1);
+
+                // 窗口缩小继续往下找
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
