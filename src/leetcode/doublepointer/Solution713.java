f131ea724271ebe0f1ddab8558e32a5c06b97c5a
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2023-02-26 11:05:32 +0800

    713. 乘积小于 K 的子数组

---
+++ src/leetcode/doublepointer/Solution713.java
@@ -0,0 +1,30 @@
+package leetcode.doublepointer;
+
+public class Solution713 {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k == 0 || k == 1) {
+            return 0;
+        }
+
+        int res = 0;
+        // 确定窗口左值开始滑动，之后不断地切换窗口大小
+        for (int left = 0; left < nums.length; left++) {
+            int sum = 1;
+            int right = left;
+
+            while (right < nums.length) {
+                sum *= nums[right];
+                if (sum < k) {
+                    res++;
+                    // 窗口后滑变大
+                    right++;
+                } else {
+                    // 窗口值超过指定值
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
