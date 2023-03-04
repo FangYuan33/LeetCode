1c675db8961c0e90f4536950af85876cdd5b5efd
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-03-04 22:22:12 +0800

    102. 二叉树的层序遍历

---
+++ src/leetcode/tree/levelorder/Solution102.java
@@ -0,0 +1,41 @@
+package leetcode.tree.levelorder;
+
+import leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> element = new ArrayList<>(size);
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                element.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            res.add(element);
+        }
+
+        return res;
+    }
+}
