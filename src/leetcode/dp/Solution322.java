fa21f361e0fc2bb3aa7364e8a05c12991cad9521
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-04-06 12:42:49 +0800

    322. 零钱兑换

--- src/leetcode/dp/Solution322.java
+++ src/leetcode/dp/Solution322.java
@@ -4,16 +4,13 @@ import java.util.Arrays;
 
 public class Solution322 {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
-        }
-
         // base case 每个索引对应金额，初始情况大于对应金额即可
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         // 初始0为0 表示正好凑齐的都到这个位置，而正好凑齐不需要累加
         dp[0] = 0;
 
+        // 初始化每个金额所需的因硬币数量
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
                 if (i >= coin) {
