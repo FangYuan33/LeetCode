171150ff0b3cfb3498db68c2c4ed10d256c061b4
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-04-20 12:38:33 +0800

    剑指 Offer 49. 丑数

--- src/leetcode/dp/SolutionOffer49.java
+++ src/leetcode/dp/SolutionOffer49.java
@@ -2,27 +2,34 @@ package leetcode.dp;
 
 public class SolutionOffer49 {
     public int nthUglyNumber(int n) {
+        if (n <= 6) {
+            return n;
+        }
+
         // base case
         int[] dp = new int[n + 1];
         dp[1] = 1;
+        // 索引值
+        int a2 = 1;
+        int b3 = 1;
+        int c5 = 1;
 
-        int a = 1;
-        int b = 1;
-        int c = 1;
-        for (int i = 2; i <= n; i++) {
-            int a2 = dp[a] * 2;
-            int b3 = dp[b] * 3;
-            int c5 = dp[c] * 5;
+        for (int i = 2; i < dp.length; i++) {
+            int a = dp[a2] * 2;
+            int b = dp[b3] * 3;
+            int c = dp[c5] * 5;
 
-            dp[i] = Math.min(a2, Math.min(b3, c5));
-            if (dp[i] == a2) {
-                a++;
+            dp[i] = Math.min(a, Math.min(b, c));
+
+            // 是并列的条件，因为可能会重复
+            if (dp[i] == a) {
+                a2++;
             }
-            if (dp[i] == b3) {
-                b++;
+            if (dp[i] == b) {
+                b3++;
             }
-            if (dp[i] == c5) {
-                c++;
+            if (dp[i] == c) {
+                c5++;
             }
         }
 
