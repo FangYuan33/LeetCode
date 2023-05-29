369421f0d917a90501ef7eda316ee130d1df10e0
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-05-29 12:49:01 +0800

    78. 子集 中等

--- src/leetcode/backtrack/Solution78.java
+++ src/leetcode/backtrack/Solution78.java
@@ -5,27 +5,29 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Solution78 {
+
     List<List<Integer>> res;
 
     public List<List<Integer>> subsets(int[] nums) {
-        this.res = new ArrayList<>();
+        res = new ArrayList<>();
         res.add(new ArrayList<>());
-        backtrack(new LinkedList<>(), nums, 0);
+
+        backtrack(nums, new LinkedList<>(), 0);
 
         return res;
     }
 
-    private void backtrack(LinkedList<Integer> element, int[] nums, int begin) {
+    private void backtrack(int[] nums, LinkedList<Integer> element, int beginIndex) {
         // 结束条件
-        if (begin == nums.length) {
+        if (beginIndex == nums.length) {
             return;
         }
 
-        for (int i = begin; i < nums.length; i++) {
+        for (int i = beginIndex; i < nums.length; i++) {
             element.addLast(nums[i]);
             res.add((List) element.clone());
 
-            backtrack(element, nums, i + 1);
+            backtrack(nums, element, i + 1);
 
             element.removeLast();
         }
