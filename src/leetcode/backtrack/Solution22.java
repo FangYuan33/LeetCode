1b6df028132bdce2530366123b9ae6cd1c75d206
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-05-30 12:43:01 +0800

    22. 括号生成

--- src/leetcode/backtrack/Solution22.java
+++ src/leetcode/backtrack/Solution22.java
@@ -8,27 +8,37 @@ public class Solution22 {
 
     public List<String> generateParenthesis(int n) {
         res = new ArrayList<>();
+
         backtrack(new StringBuilder(), n, n);
 
         return res;
     }
 
     private void backtrack(StringBuilder element, int left, int right) {
-        // 结束条件 左右括号都用完了
+        // 结束条件
         if (left == 0 && right == 0) {
             res.add(element.toString());
             return;
         }
 
-        // 做选择，是贴左括号还是贴右括号
+        // 做选择，先狂加左括号
         if (left > 0) {
+            left--;
             element.append("(");
-            backtrack(element, left - 1, right);
+
+            backtrack(element, left, right);
+
+            left++;
             element.deleteCharAt(element.length() - 1);
         }
-        if (right > 0 && right > left) {
+        // 再加右括号，注意需要右值大于左值才能加
+        if (right > 0 && left < right) {
+            right--;
             element.append(")");
-            backtrack(element, left, right - 1);
+
+            backtrack(element, left, right);
+
+            right++;
             element.deleteCharAt(element.length() - 1);
         }
     }
