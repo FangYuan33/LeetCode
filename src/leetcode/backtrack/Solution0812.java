9dc09bc92ae3516ecb9012db224d3cdade2ab3f1
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-05-31 20:59:29 +0800

    面试题 08.12. 八皇后

--- src/leetcode/backtrack/Solution0812.java
+++ src/leetcode/backtrack/Solution0812.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution0812 {
-
     List<List<String>> res;
     int n;
 
@@ -14,77 +13,81 @@ public class Solution0812 {
 
         // 初始化棋盘
         char[][] locations = new char[n][n];
-        for (int i = 0; i < locations.length; i++) {
-            for (int j = 0; j < locations[0].length; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 locations[i][j] = '.';
             }
         }
-        // 执行回溯
+
         backtrack(locations, 0);
 
         return res;
     }
 
-    // rowNum正要选择的行数
-    private void backtrack(char[][] locations, int rowNum) {
-        // 结束条件: 遍历完最后一行
-        if (rowNum == n) {
-            List<String> element = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
-                element.add(new String(locations[j]));
+    private void backtrack(char[][] locations, int row) {
+        // 结束条件：遍历到了最后一行
+        if (row == n) {
+            // 把棋盘的每行都拿出来初始化该答案
+            ArrayList<String> element = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                element.add(String.valueOf(locations[i]));
             }
+
             res.add(element);
 
             return;
         }
 
-        // 在每行中选皇后的位置
-        for (int i = 0; i < n; i++) {
-            // 该位置选皇后还是不选皇后
-            if (isValid(locations, rowNum, i)) {
-                locations[rowNum][i] = 'Q';
-                backtrack(locations, rowNum + 1);
-                // 回溯完该行的这个位置选择后，将其重置成"."
-                locations[rowNum][i] = '.';
+        // 开始在棋盘上做选择，放皇后还是不放皇后
+        for (int j = 0; j < n; j++) {
+            // 能放的话则放皇后
+            if (isValid(locations, row, j)) {
+                locations[row][j] = 'Q';
+
+                // 继续向下一行看能不能放皇后
+                backtrack(locations, row + 1);
+
+                // 回溯完了需要把该位置的皇后拿掉
+                locations[row][j] = '.';
             }
-            // 不选皇后不再向下执行
         }
     }
 
-    // 判断该位置是否能放置皇后
-    private boolean isValid(char[][] locations, int row, int column) {
-        // 校验上一行有皇后
-        boolean flag = false;
+    // 判断是否能放皇后
+    private boolean isValid(char[][] locations, int row, int colume) {
+        // 先判断上一行是否有皇后，没皇后直接false，第一行开始放默认有皇后
         if (row == 0) {
-            flag = true;
+            return true;
         }
-        for (int i = 0; i < n && row >= 1; i++) {
-            if (locations[row - 1][i] == 'Q') {
-                flag = true;
+        boolean exist = false;
+        for (int j = 0; j < n; j++) {
+            if (locations[row - 1][j] == 'Q') {
+                exist = true;
                 break;
             }
         }
+        if (!exist) {
+            return false;
+        }
 
-        // 校验同列不能有皇后
+        // 判断同列上有没有皇后
         for (int i = row - 1; i >= 0; i--) {
-            if (locations[i][column] == 'Q') {
+            if (locations[i][colume] == 'Q') {
                 return false;
             }
         }
-
-        // 校验对角线不能有皇后
-        // 列的偏移量
+        // 判断对角线上有没有皇后，指定 x 为偏移量
         int x = 1;
         for (int i = row - 1; i >= 0; i--, x++) {
-            if (column - x >= 0 && locations[i][column - x] == 'Q') {
+            if (colume + x < n && locations[i][colume + x] == 'Q') {
                 return false;
             }
-            if (column + x <= n - 1 && locations[i][column + x] == 'Q') {
+            if (colume - x >= 0 && locations[i][colume - x] == 'Q') {
                 return false;
             }
         }
 
-        return flag;
+        return true;
     }
 
     public static void main(String[] args) {
