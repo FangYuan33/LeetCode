ee8590eeee079dc8699da4a997de1db32739d98e
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-06-01 12:58:47 +0800

    10. 正则表达式匹配

--- src/leetcode/backtrack/Solution10.java
+++ src/leetcode/backtrack/Solution10.java
@@ -14,28 +14,29 @@ public class Solution10 {
             }
         }
 
-        // *的判断 * 要求前边怎么都得有个字符才行所以得加1
+        // *的判断 * 要求前边得有个字符才行，所以得加1，此时的正则索引是*的前一个索引
         if (pIndex < (p.length() - 1) && p.charAt(pIndex + 1) == '*') {
-            // 多个匹配
+            // 发生匹配情况
             if (sIndex < s.length() && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '.')) {
-                // 不断把字符串 + 1 去核对
+                // 继续向下判断字符串是否匹配，正则索引不改变
                 boolean res1 = backtrack(s, sIndex + 1, p, pIndex);
-                // 字符串 + 1核对完了，正则表达式往后移动2位看看到没到末尾
+                // 字符串匹配完了，正则索引 + 2 移动到和正则表达式一样长度，来看看能否匹配成功
                 boolean res2 = backtrack(s, sIndex, p, pIndex + 2);
                 return res1 || res2;
             }
-            // 0个匹配
+            // 没有发生匹配的情况
             return backtrack(s, sIndex, p, pIndex + 2);
         }
-        // .的判断
-        if (pIndex < p.length() && sIndex < s.length() && p.charAt(pIndex) == '.') {
-            return backtrack(s, sIndex + 1, p, pIndex + 1);
-        }
-        // 字符的判断
-        if (pIndex < p.length() && sIndex < s.length() && s.charAt(sIndex) == p.charAt(pIndex)) {
+        // . 和 字符 的匹配逻辑是一样的，都需要向后移动一位索引值来继续匹配
+        if (sIndex < s.length() && pIndex < p.length() && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '.')) {
             return backtrack(s, sIndex + 1, p, pIndex + 1);
         }
 
+        // 字符不等的话返回false
         return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution10().isMatch("ab", ".*"));
     }
 }
