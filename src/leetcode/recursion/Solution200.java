2588847c0e0b62cb9232b775458fcb01227c6854
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-06-09 12:28:50 +0800

    200. 岛屿数量

--- src/leetcode/recursion/Solution200.java
+++ src/leetcode/recursion/Solution200.java
@@ -1,10 +1,11 @@
 package leetcode.recursion;
 
 public class Solution200 {
+    /**
+     * 对整个矩阵都进行遍历，如果遇到了岛屿，那么把这块岛屿通过递归来变成海洋
+     * 这就可以在之后遍历的时候保证不会重复统计
+     */
     public int numIslands(char[][] grid) {
-        // 对整个矩阵都进行遍历，如果遇到了岛屿，那么把这块岛屿通过递归来变成海洋
-        // 那么这就可以在之后遍历的时候保证不会重复统计
-
         int res = 0;
 
         for (int i = 0; i < grid.length; i++) {
@@ -12,7 +13,7 @@ public class Solution200 {
                 if (grid[i][j] == '1') {
                     res++;
                     // 清除该岛屿
-                    recur(grid, i, j);
+                    recursion(grid, i, j);
                 }
             }
         }
@@ -20,7 +21,7 @@ public class Solution200 {
         return res;
     }
 
-    private void recur(char[][] grid, int row, int column) {
+    private void recursion(char[][] grid, int row, int column) {
         // 结束条件 越界、遇到海洋
         if (row < 0 || row >= grid.length || column < 0 || column >= grid[0].length
                 || grid[row][column] == '0') {
@@ -30,10 +31,10 @@ public class Solution200 {
         // 陆地变海洋
         grid[row][column] = '0';
 
-        // 上下左右再去变
-        recur(grid, row + 1, column);
-        recur(grid, row - 1, column);
-        recur(grid, row, column + 1);
-        recur(grid, row, column - 1);
+        // 上下左右递归
+        recursion(grid, row + 1, column);
+        recursion(grid, row - 1, column);
+        recursion(grid, row, column + 1);
+        recursion(grid, row, column - 1);
     }
 }
