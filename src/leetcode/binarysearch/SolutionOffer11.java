4e771180032d207088ad07464d11e71f6997127f
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-06-12 21:10:54 +0800

    剑指 Offer 11. 旋转数组的最小数字

--- src/leetcode/binarysearch/SolutionOffer11.java
+++ src/leetcode/binarysearch/SolutionOffer11.java
@@ -2,9 +2,9 @@ package leetcode.binarysearch;
 
 public class SolutionOffer11 {
     public int minArray(int[] numbers) {
+        // 寻找数据的两段性
         int left = 0, right = numbers.length - 1;
-
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
 
             if (numbers[mid] > numbers[right]) {
@@ -14,10 +14,26 @@ public class SolutionOffer11 {
                 // 落在了小区间
                 right = mid;
             } else {
-                right--;
+                // 不知道是落在了大区间还是小区间，答案可能在中值左边也可能在右边
+                // 而且一定存在 [left, mid] 都相等 或 [mid, right] 的值都相等
+                // 前者都相等的话，答案出现在右边儿；后者相等的话，答案在左边儿 那么答案一定在[left, right]这个区间内
+                // 以left为索引执行线性遍历找到这个值即可
+                int index = left;
+
+                for (int i = index + 1; i < right; i++) {
+                    if (numbers[i] < numbers[index]) {
+                        index = i;
+                    }
+                }
+
+                return numbers[index];
             }
         }
 
-        return numbers[left];
+        return numbers[right];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SolutionOffer11().minArray(new int[]{3, 3, 1, 3}));
     }
 }
