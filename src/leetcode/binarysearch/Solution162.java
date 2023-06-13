4a668ef054ab15e55bc3d47b931a2e1a0a2832f6
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-06-13 12:33:01 +0800

    162. 寻找峰值

--- src/leetcode/binarysearch/Solution162.java
+++ src/leetcode/binarysearch/Solution162.java
@@ -7,12 +7,13 @@ public class Solution162 {
         while (left < right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] > nums[mid + 1]) {
-                // 升
-                right = mid;
-            } else {
-                // 降
+            // 不断靠近山峰
+            if (nums[mid] < nums[right]) {
+                // 中值比右侧小，说明右侧一定有山峰，那么缩小区间
                 left = mid + 1;
+            } else if (nums[mid] >= nums[right]) {
+                // 中值比右侧大，但是又不能确定右侧有，所以慢慢地缩小区间
+                right -= 1;
             }
         }
 
