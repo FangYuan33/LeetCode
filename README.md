6bc2cdbe28630a4e515d9f95277d8cbae549e570
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-06-13 12:13:52 +0800

    154. 寻找旋转排序数组中的最小值 II

--- README.md
+++ README.md
@@ -309,8 +309,7 @@ def backtrack(路径, 选择列表):
 
 1. [704. 二分查找 简单](https://leetcode.cn/problems/binary-search/)
 2. [剑指 Offer 53 - I. 在排序数组中查找数字 I 简单](https://leetcode.cn/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/)
-3. [剑指 Offer 11. 旋转数组的最小数字 简单](https://leetcode.cn/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/)
-4. [154. 寻找旋转排序数组中的最小值 II 困难](https://leetcode.cn/problems/find-minimum-in-rotated-sorted-array-ii/)
+3. [154. 寻找旋转排序数组中的最小值 II 困难](https://leetcode.cn/problems/find-minimum-in-rotated-sorted-array-ii/)
 
 ### 数组无序
 
--- src/leetcode/binarysearch/Solution154.java
+++ src/leetcode/binarysearch/Solution154.java
@@ -8,24 +8,35 @@ public class Solution154 {
 //        int[] nums = new int[]{2, 2, 2, 0, 1};
         int[] nums = new int[]{3, 3, 1, 3};
 
-        System.out.println(findMin(nums));
+        System.out.println(new Solution154().findMin(nums));
     }
 
-    private static int findMin(int[] nums) {
+    public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
 
         while (left < right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
 
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else if (nums[mid] < nums[right]) {
+            if (nums[mid] < nums[right]) {
+                // 落在了小区间
                 right = mid;
+            } else if (nums[mid] > nums[right]) {
+                // 落在了大区间
+                left = mid + 1;
             } else {
-                right--;
+                // 相等情况，那么需要线性查找，而且存在左右两个区间都是相等的值的情况
+                // 它可能在mid的左右两侧，答案一定在[left, right]之间
+                int index = left;
+                for (int i = left + 1; i < right; i++) {
+                    if (nums[i] < nums[index]) {
+                        index = i;
+                    }
+                }
+
+                return nums[index];
             }
         }
 
-        return nums[right];
+        return nums[left];
     }
 }
