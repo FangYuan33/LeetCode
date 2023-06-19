967de9d0d5f6f147af4eaccad95d1534fa5de8d3
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-06-19 12:35:33 +0800

    剑指 Offer 57 - II. 和为s的连续正数序列 简单

--- src/leetcode/slidingwindow/SolutionOffer57.java
+++ src/leetcode/slidingwindow/SolutionOffer57.java
@@ -1,23 +1,44 @@
 package leetcode.slidingwindow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SolutionOffer57 {
-    public int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
-        int left = 0, right = nums.length - 1;
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> resList = new ArrayList<>();
+        // 滑动窗口
+        int left = 1, right = 1;
+        // 中值，左窗口不能超过它
+        int half = target / 2;
 
-        for (int i = 0; i < nums.length && left < right; i++) {
-            int sum = nums[left] + nums[right];
-
-            if (sum < target) {
+        int sum = 0;
+        while (left <= half) {
+            if (sum > target) {
+                // 过大，缩小窗口
+                sum -= left;
                 left++;
-            } else if (sum > target) {
-                right--;
+            } else if (sum < target) {
+                // 过小，扩大窗口
+                sum += right;
+                right++;
             } else {
-                res[0] = nums[left];
-                res[1] = nums[right];
+                resList.add(initialElement(left, right));
+
+                // 缩小窗口
+                sum -= left;
+                left++;
             }
         }
 
-        return res;
+        return resList.toArray(new int[resList.size()][]);
+    }
+
+    private int[] initialElement(int left, int right) {
+        int[] array = new int[right - left];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = left++;
+        }
+
+        return array;
     }
 }
