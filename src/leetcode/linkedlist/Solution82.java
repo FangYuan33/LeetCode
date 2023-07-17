6db7a5378998d225b9ec56f9fd61bb5e02599bca
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-07-17 20:45:36 +0800

    82. 删除排序链表中的重复元素 II

--- src/leetcode/linkedlist/Solution82.java
+++ src/leetcode/linkedlist/Solution82.java
@@ -6,21 +6,24 @@ public class Solution82 {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode pre = new ListNode(-1);
         pre.next = head;
+        // 把重复的前驱节点也记录下来，删除节点的时候使用
+        ListNode preHead = pre;
 
-        ListNode preDelete = pre;
         while (head != null) {
-            int val = head.val;
-            if (head.next != null && head.next.val == val) {
-                // 找到和重复值不等的节点
-                while (head != null && head.val == val) {
-                    head = head.next;
+            if (head.next != null && head.val == head.next.val) {
+                // 碰见重复的一直删除
+                while (head.next != null && head.val == head.next.val) {
+                    head.next = head.next.next;
                 }
-                preDelete.next = head;
-                continue;
-            }
 
-            head = head.next;
-            preDelete = preDelete.next;
+                // 删除完毕后把这个节点也删除
+                preHead.next = head.next;
+                head = preHead.next;
+            } else {
+                // 不重复的变换指针即可
+                head = head.next;
+                preHead = preHead.next;
+            }
         }
 
         return pre.next;
