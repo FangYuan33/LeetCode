210f59cb4bfc1f874ef61c0a198eccfec33b2d57
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-07-29 17:00:35 +0800

    141. 环形链表

--- src/leetcode/linkedlist/Solution141.java
+++ src/leetcode/linkedlist/Solution141.java
@@ -4,16 +4,13 @@ import leetcode.ListNode;
 
 public class Solution141 {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
-
         ListNode slow = head, fast = head;
 
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
             slow = slow.next;
-            if (fast == slow) {
+            fast = fast.next.next;
+
+            if (slow == fast) {
                 return true;
             }
         }
