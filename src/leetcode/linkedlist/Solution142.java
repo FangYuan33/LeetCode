235363a84d5d41471bf6f9b3146cc51532b07795
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-07-29 18:11:50 +0800

    142. 环形链表 II

--- src/leetcode/linkedlist/Solution142.java
+++ src/leetcode/linkedlist/Solution142.java
@@ -16,8 +16,10 @@ public class Solution142 {
             slow = slow.next;
             // 相遇说明有环
             if (slow == fast) {
-                // 推导公式出结果 头结点和慢节点一起走并相遇时可以到达环的入口
-                // 因为此时一起走并相遇正好走了n圈
+                // f 表示 fast 指针走的距离；s 表示 slow 指针走的距离；记圈中节点数为 n；
+                // 头节点到入环节点的距离为 x，那么走 n * k + x 距离始终在头节点处，其中 k 为任意正整数
+                // f = 2s, f = n * y + s 其中 y 为 fast 多走的圈数 -> s = n * y
+                // 我们可知相遇后，如果 slow 再走 x 那么即可达到入口处，如果此时 slow 和头节点一起走，相遇时则为入口节点
                 while (slow != head) {
                     slow = slow.next;
                     head = head.next;
