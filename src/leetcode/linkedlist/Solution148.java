b0b13743f9ea106dec8ede8193d8959148895919
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-07-31 16:36:55 +0800

    148. 排序链表

--- src/leetcode/linkedlist/Solution148.java
+++ src/leetcode/linkedlist/Solution148.java
@@ -3,6 +3,20 @@ package leetcode.linkedlist;
 import leetcode.ListNode;
 
 public class Solution148 {
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(4);
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(1);
+
+        node.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+
+        new Solution148().sortList(node1);
+    }
+
     // 不断地拆段后合并，因为每段拆出来后并不是升序的
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) {
@@ -10,28 +24,26 @@ public class Solution148 {
         }
 
         // 拆分成两段
-        ListNode res = new ListNode(-1);
-        res.next = head;
-        ListNode slow = res;
-        ListNode fast = res;
+        ListNode pre = new ListNode(-1);
+        pre.next = head;
+        ListNode slow = pre, fast = pre;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode rightHead = slow.next;
+        // 找到位置后将链表断开
+        ListNode halfHead = slow.next;
         slow.next = null;
 
-        // 继续拆
-        ListNode left = sortList(res.next);
-        ListNode right = sortList(rightHead);
+        ListNode left = sortList(pre.next);
+        ListNode right = sortList(halfHead);
 
-        // 合并
         return merge(left, right);
     }
 
     private ListNode merge(ListNode left, ListNode right) {
         if (left == null || right == null) {
-            return left != null ? left : right;
+            return left == null ? right : left;
         }
 
         if (left.val <= right.val) {
