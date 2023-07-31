c10e937e37b4cb1acfa171db69e4091275fde200
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-07-31 14:11:54 +0800

    1206. 设计跳表

--- src/leetcode/datastructure/Skiplist.java
+++ src/leetcode/datastructure/Skiplist.java
@@ -2,8 +2,16 @@ package leetcode.datastructure;
 
 import java.util.Random;
 
+/**
+ * Skiplist class represents a skip list data structure.
+ */
 public class Skiplist {
 
+    /**
+     * The main method creates a Skiplist object, adds elements to it, and searches for an element.
+     *
+     * @param args The command-line arguments.
+     */
     public static void main(String[] args) {
         Skiplist skiplist = new Skiplist();
 
@@ -13,45 +21,88 @@ public class Skiplist {
 
         System.out.println(skiplist.search(1));
     }
+
     int level = 10;
+
+    /**
+     * Node class represents a node in the skip list.
+     */
     class Node {
         int val;
         Node[] ne = new Node[level];
-        Node (int _val) {
+
+        /**
+         * Constructs a new Node with the given value.
+         *
+         * @param _val The value to be stored in the node.
+         */
+        Node(int _val) {
             val = _val;
         }
     }
+
     Random random = new Random();
     Node he = new Node(-1);
+
+    /**
+     * Helper method to find nodes in the skip list.
+     *
+     * @param t  The target value to search for.
+     * @param ns An array of nodes to store the found nodes at each level.
+     */
     void find(int t, Node[] ns) {
         Node cur = he;
         for (int i = level - 1; i >= 0; i--) {
-            while (cur.ne[i] != null && cur.ne[i].val < t) cur = cur.ne[i];
+            while (cur.ne[i] != null && cur.ne[i].val < t)
+                cur = cur.ne[i];
             ns[i] = cur;
         }
     }
+
+    /**
+     * Searches for a value in the skip list.
+     *
+     * @param t The value to search for.
+     * @return True if the value is found, false otherwise.
+     */
     public boolean search(int t) {
         Node[] ns = new Node[level];
         find(t, ns);
         return ns[0].ne[0] != null && ns[0].ne[0].val == t;
     }
+
+    /**
+     * Inserts a value into the skip list.
+     *
+     * @param t The value to be inserted.
+     */
     public void add(int t) {
         Node[] ns = new Node[level];
         find(t, ns);
         Node node = new Node(t);
         for (int i = 0; i < level; i++) {
-            // 当前节点保存每一层的当前节点的下一个节点的指针
+            // Saves the pointer to the next node at each level for the current node
             node.ne[i] = ns[i].ne[i];
             ns[i].ne[i] = node;
-            if (random.nextInt(2) == 0) break;
+            if (random.nextInt(2) == 0)
+                break;
         }
     }
+
+    /**
+     * Removes a value from the skip list.
+     *
+     * @param t The value to be removed.
+     * @return True if the value is removed, false if the value is not found.
+     */
     public boolean erase(int t) {
         Node[] ns = new Node[level];
         find(t, ns);
         Node node = ns[0].ne[0];
-        if (node == null || node.val != t) return false;
-        for (int i = 0; i < level && ns[i].ne[i] == node; i++) ns[i].ne[i] = ns[i].ne[i].ne[i];
+        if (node == null || node.val != t)
+            return false;
+        for (int i = 0; i < level && ns[i].ne[i] == node; i++)
+            ns[i].ne[i] = ns[i].ne[i].ne[i];
         return true;
     }
 }
