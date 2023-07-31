9a6e773a9f4400e639cd09884ff2fd8dfec23c3e
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-07-31 14:22:44 +0800

    234. 回文链表

--- src/leetcode/linkedlist/Solution234.java
+++ src/leetcode/linkedlist/Solution234.java
@@ -4,42 +4,34 @@ import leetcode.ListNode;
 
 public class Solution234 {
     public boolean isPalindrome(ListNode head) {
-        // 快慢指针分割出链表
-        ListNode slow = new ListNode(-1);
-        slow.next = head;
-        ListNode fast = slow;
-
+        // 快慢指针分开
+        ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // slow的下一个节点即为右链表
-        ListNode rightStart = slow.next;
-        slow.next = null;
-        // 反转右侧
-        ListNode rightHead = reverse(rightStart);
-
-        // 以右侧为准，因为奇数个的时候可能右侧少一个节点
-        while (rightHead != null) {
-            if (rightHead.val != head.val) {
+        ListNode halfHead = reverse(slow);
+        while (halfHead != null && head != null) {
+            if (halfHead.val != head.val) {
                 return false;
             }
-            rightHead = rightHead.next;
+            halfHead = halfHead.next;
             head = head.next;
         }
 
         return true;
     }
 
-    private ListNode reverse(ListNode head) {
+    private ListNode reverse(ListNode node) {
         ListNode pre = null;
 
-        while (head != null) {
-            ListNode temp = head.next;
-            head.next = pre;
-            pre = head;
-            head = temp;
+        while (node != null) {
+            ListNode temp = node.next;
+
+            node.next = pre;
+            pre = node;
+            node = temp;
         }
 
         return pre;
