af310141320fa256789812e85c80abe53d87fa8a
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-21 22:00:13 +0800

    729. 我的日程安排表 I 线段树解法

--- README.md
+++ README.md
@@ -125,7 +125,7 @@
 |    | [1893. 检查是否区域内所有整数都被覆盖 简单](https://leetcode.cn/problems/check-if-all-the-integers-in-a-range-are-covered/) | [Solution1893.java](src%2Fleetcode%2Fsegmenttree%2FSolution1893.java) |      |
 |    | [654. 最大二叉树 中等](https://leetcode.cn/problems/maximum-binary-tree/)                                         | [Solution654.java](src%2Fleetcode%2Fsegmenttree%2FSolution654.java)   |      |
 |    | [1109. 航班预订统计 中等](https://leetcode.cn/problems/corporate-flight-bookings/)                                 | [Solution1109.java](src%2Fleetcode%2Fsegmenttree%2FSolution1109.java) | 区间修改 |
-|    | [729. 我的日程安排表 I 中等](https://leetcode.cn/problems/my-calendar-i/)                                           |                                                                       |      |
+|    | [729. 我的日程安排表 I 中等](https://leetcode.cn/problems/my-calendar-i/)                                           | [MyCalendar.java](src%2Fleetcode%2Fsegmenttree%2FMyCalendar.java)     |      |
 |    | [731. 我的日程安排表 II 中等](https://leetcode.cn/problems/my-calendar-ii/)                                         |                                                                       |      |
 |    | [732. 我的日程安排表 III 困难](https://leetcode.cn/problems/my-calendar-iii/)                                       |                                                                       |      |
 |    | [327. 区间和的个数 困难](https://leetcode.cn/problems/count-of-range-sum/)                                         |                                                                       |      |
--- src/leetcode/segmenttree/MyCalendar.java
+++ src/leetcode/segmenttree/MyCalendar.java
@@ -2,11 +2,115 @@ package leetcode.segmenttree;
 
 public class MyCalendar {
 
-    public MyCalendar() {
+    private SegmentTree segmentTree;
 
+    public MyCalendar() {
+        segmentTree = new SegmentTree();
     }
 
     public boolean book(int start, int end) {
+        if (segmentTree.query(1, 1, (int) 1e9, start, end - 1) > 0) {
+            return false;
+        }
+
+        segmentTree.update(1, 1, (int) 1e9, start, end - 1, 1);
+
         return true;
+    }
+
+    static class SegmentTree {
+        static class Node {
+            int left;
+
+            int right;
+
+            int val;
+
+            int add;
+
+
+        }
+
+        Node[] tree;
+        int count;
+
+        public SegmentTree() {
+            count = 1;
+            this.tree = new Node[(int) 1e5];
+        }
+
+        public int query(int pos, int left, int right, int l, int r) {
+            if (l <= left && right <= r) {
+                return tree[pos].val;
+            }
+
+            lazyCreate(pos);
+
+            pushDown(pos, right - left + 1);
+
+            int res = 0;
+            int mid = left + right >> 1;
+            if (l <= mid) {
+                res += query(tree[pos].left, left, mid, l, r);
+            }
+            if (r > mid) {
+                res += query(tree[pos].right, mid + 1, right, l, r);
+            }
+
+            return res;
+        }
+
+        public void update(int pos, int left, int right, int l, int r, int val) {
+            if (l <= left && right <= r) {
+                tree[pos].val += val * (right - left + 1);
+                tree[pos].add += val;
+                return;
+            }
+
+            lazyCreate(pos);
+
+            pushDown(pos, right - left + 1);
+
+            int mid = left + right >> 1;
+            if (l <= mid) {
+                update(tree[pos].left, left, mid, l, r, val);
+            }
+            if (r > mid) {
+                update(tree[pos].right, mid + 1, right, l, r, val);
+            }
+
+            pushUp(pos);
+        }
+
+        private void lazyCreate(int pos) {
+            if (tree[pos] == null) {
+                tree[pos] = new Node();
+            }
+
+            if (tree[pos].left == 0) {
+                tree[pos].left = ++count;
+                tree[tree[pos].left] = new Node();
+            }
+            if (tree[pos].right == 0) {
+                tree[pos].right = ++count;
+                tree[tree[pos].right] = new Node();
+            }
+        }
+
+        private void pushDown(int pos, int len) {
+            if (tree[pos].left != 0 && tree[pos].right != 0 && tree[pos].add != 0) {
+                tree[tree[pos].left].val += tree[pos].add * (len - len / 2);
+                tree[tree[pos].right].val += tree[pos].add * len / 2;
+
+                tree[tree[pos].left].add += tree[pos].add;
+                tree[tree[pos].right].add += tree[pos].add;
+
+                tree[pos].add = 0;
+            }
+        }
+
+        private void pushUp(int pos) {
+            tree[pos].val = tree[tree[pos].left].val + tree[tree[pos].right].val;
+        }
     }
 }
