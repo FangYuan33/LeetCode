607c264cc3c2aae2deaa797a29b96cbf0b968a9c
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-23 21:08:37 +0800

    1395. 统计作战单位数 review

--- src/leetcode/binaryindexedtree/Solution1395.java
+++ src/leetcode/binaryindexedtree/Solution1395.java
@@ -2,25 +2,28 @@ package leetcode.binaryindexedtree;
 
 public class Solution1395 {
 
-    // 使用线段树在长为 1e5 标记出现过的数字
-    // 那么这样在发生 大于或小于 时，统计这个区间内被标记为 1 的数即可
+    public static void main(String[] args) {
+        System.out.println(new Solution1395().numTeams(new int[]{2, 5, 3, 4, 1}));
+    }
+
     public int numTeams(int[] rating) {
-        BinaryIndexedTree binaryIndexedTree = new BinaryIndexedTree();
         int res = 0;
 
+        BinaryIndexedTree tree = new BinaryIndexedTree();
         for (int i = 0; i < rating.length; i++) {
             for (int j = i + 1; j < rating.length; j++) {
-                if (rating[i] < rating[j]) {
-                    res += binaryIndexedTree.query(rating[i], rating[j]);
-                } else {
-                    res += binaryIndexedTree.query(rating[j], rating[i]);
+                if (rating[j] > rating[i]) {
+                    res += tree.query(rating[i], rating[j]);
+                }
+                if (rating[j] < rating[i]) {
+                    res += tree.query(rating[j], rating[i]);
                 }
 
-                binaryIndexedTree.update(rating[j], 1);
+                tree.update(rating[j], 1);
             }
-            // 遍历完本组数后把树状数组恢复
+
             for (int j = i + 1; j < rating.length; j++) {
-                binaryIndexedTree.update(rating[j], -1);
+                tree.update(rating[j], -1);
             }
         }
 
@@ -28,36 +31,33 @@ public class Solution1395 {
     }
 
     static class BinaryIndexedTree {
-
-        private static final int N = (int) 1e5 + 1;
-
         int[] tree;
 
         public BinaryIndexedTree() {
-            this.tree = new int[N];
+            this.tree = new int[(int) 1e5 + 1];
         }
 
-        public void update(int num, int val) {
-            for (int i = num; i < N; i += lowbit(i)) {
+        public void update(int rating, int val) {
+            for (int i = rating; i < tree.length; i += lowbit(i)) {
                 tree[i] += val;
             }
         }
 
         public int query(int left, int right) {
-            return query(right - 1) - query(left);
+            return query(right) - query(left - 1);
         }
 
-        private int query(int n) {
+        public int query(int num) {
             int res = 0;
-            for (int i = n; i > 0; i -= lowbit(i)) {
+            for (int i = num; i > 0; i -= lowbit(i)) {
                 res += tree[i];
             }
 
             return res;
         }
 
-        private int lowbit(int x) {
-            return x & -x;
+        private int lowbit(int i) {
+            return i & -i;
         }
     }
 }
