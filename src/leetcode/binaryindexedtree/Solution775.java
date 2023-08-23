134c0173ba4a4c03b59ec08b1fa6ab1eb1aa6078
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-23 20:30:39 +0800

    775. 全局倒置与局部倒置 review

--- src/leetcode/binaryindexedtree/Solution775.java
+++ src/leetcode/binaryindexedtree/Solution775.java
@@ -2,51 +2,51 @@ package leetcode.binaryindexedtree;
 
 public class Solution775 {
 
-    public static void main(String[] args) {
-        System.out.println(new Solution775().isIdealPermutation(new int[]{1, 2, 0}));
-    }
-
-    // 本题的解题关键是查找逆序对，那么我们计算某个数前边出现过多少个比它大的值就可以了
+    // 局部倒置比较好算，遍历的时候计算前边的是否比当前的大就可以
+    // 全局倒置查找当前节点前比自己大的节点的个数，本质上两种都是计算逆序对
     public boolean isIdealPermutation(int[] nums) {
-        // 全局
-        int count1 = 0;
-        // 局部
-        int count2 = 0;
-        BinaryIndexedTree binaryIndexedTree = new BinaryIndexedTree(nums.length);
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] < nums[i - 1]) {
-                count2++;
-            }
-            count1 += binaryIndexedTree.queryNum(nums[i]);
-            binaryIndexedTree.update(nums[i]);
+        if (nums.length == 1) {
+            return true;
         }
 
-        return count1 == count2;
+        BinaryIndexedTree tree = new BinaryIndexedTree(nums.length);
+        tree.update(nums[0] + 1);
+
+        int local = 0;
+        int all = 0;
+        for (int i = 1; i < nums.length; i++) {
+            // 统计局部倒置
+            if (nums[i - 1] > nums[i]) {
+                local++;
+            }
+
+            all += tree.queryNum(nums[i] + 1);
+            tree.update(nums[i] + 1);
+        }
+
+        return local == all;
     }
 
-    class BinaryIndexedTree {
+    // 因为数值范围是 [0, n - 1]，那么我们可以标记具体数字有没有出现，出现即为 1，未出现则为 0，这样我们就能查询出比当前节点大的数字的数量了
+    static class BinaryIndexedTree {
 
         int[] tree;
 
         public BinaryIndexedTree(int n) {
-            this.tree =  new int[n + 1];
+            this.tree = new int[n + 1];
         }
 
-        // 记录的是 num + 1 位置的数量，否则 num 为 0 时会死循环
         public void update(int num) {
-            for (int i = num + 1; i < tree.length; i += lowbit(i)) {
-                tree[i]++;
+            for (int i = num; i < tree.length; i += lowbit(i)) {
+                tree[i] += 1;
             }
         }
 
-        // 查询的是当前数之后的数量
         public int queryNum(int num) {
             return query(tree.length - 1) - query(num);
         }
 
-
-        private int query(int num) {
+        public int query(int num) {
             int res = 0;
             for (int i = num; i > 0; i -= lowbit(i)) {
                 res += tree[i];
