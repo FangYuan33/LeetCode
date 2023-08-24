cf619b2f4bd94eb85229d141bbafe8fb80ef16db
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-24 21:14:54 +0800

    前缀和 review

--- src/leetcode/prefixsum/NumMatrix.java
+++ src/leetcode/prefixsum/NumMatrix.java
@@ -13,7 +13,6 @@ public class NumMatrix {
     public NumMatrix(int[][] matrix) {
         sum = new int[matrix.length + 1][matrix[0].length + 1];
 
-        // 预先计算前缀和
         for (int i = 1; i < sum.length; i++) {
             for (int j = 1; j < sum[0].length; j++) {
                 sum[i][j] = matrix[i - 1][j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
@@ -22,7 +21,7 @@ public class NumMatrix {
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        row1++; col1++; row2++; col2++;
+        row1++; row2++; col1++; col2++;
         return sum[row2][col2] - sum[row1 - 1][col2] - sum[row2][col1 - 1] + sum[row1 - 1][col1 - 1];
     }
 
--- src/leetcode/prefixsum/Solution396.java
+++ src/leetcode/prefixsum/Solution396.java
@@ -8,29 +8,27 @@ public class Solution396 {
 
     public int maxRotateFunction(int[] nums) {
         int n = nums.length;
-        // 计算双倍长度的前缀和
+        // 计算前缀和
         int[] sum = new int[n * 2];
         sum[0] = nums[0];
         for (int i = 1; i < sum.length; i++) {
             sum[i] = sum[i - 1] + nums[i % n];
         }
 
-        // 计算出第一个答案，然后递推之后的
-        int ans = 0;
+        // 计算第一组值
+        int res = 0;
         for (int i = 1; i < nums.length; i++) {
-            ans += nums[i] * i;
+            res += i * nums[i];
         }
 
-        int temp = ans;
+        int temp = res;
         for (int i = 0; i < nums.length - 1; i++) {
-            // 先加上右边扩展的值
-            temp += nums[i] * (n - 1);
-            // 再减去中间变化的值
+            temp += (n - 1) * nums[i];
             temp -= sum[n - 1 + i] - sum[i];
 
-            ans = Math.max(ans, temp);
+            res = Math.max(res, temp);
         }
 
-        return ans;
+        return res;
     }
 }
