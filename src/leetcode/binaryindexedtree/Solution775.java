526fcdb0983c6e64f051ea4a4fcb3b8a9fe8a63c
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-24 09:08:16 +0800

    775. 全局倒置与局部倒置 review

--- src/leetcode/binaryindexedtree/Solution775.java
+++ src/leetcode/binaryindexedtree/Solution775.java
@@ -2,32 +2,30 @@ package leetcode.binaryindexedtree;
 
 public class Solution775 {
 
-    // 局部倒置比较好算，遍历的时候计算前边的是否比当前的大就可以
-    // 全局倒置查找当前节点前比自己大的节点的个数，本质上两种都是计算逆序对
     public boolean isIdealPermutation(int[] nums) {
         if (nums.length == 1) {
             return true;
         }
 
+        // 出现的数字全部入线段树，查询到的以当前数为左边界开区间的数量即为全局倒置数量
+        int all = 0;
+        int local = 0;
         BinaryIndexedTree tree = new BinaryIndexedTree(nums.length);
         tree.update(nums[0] + 1);
 
-        int local = 0;
-        int all = 0;
         for (int i = 1; i < nums.length; i++) {
-            // 统计局部倒置
             if (nums[i - 1] > nums[i]) {
                 local++;
             }
+            all += tree.queryNm(nums[i] + 1);
 
-            all += tree.queryNum(nums[i] + 1);
             tree.update(nums[i] + 1);
         }
 
         return local == all;
     }
 
-    // 因为数值范围是 [0, n - 1]，那么我们可以标记具体数字有没有出现，出现即为 1，未出现则为 0，这样我们就能查询出比当前节点大的数字的数量了
+    // 涉及单点更新和区间查询
     static class BinaryIndexedTree {
 
         int[] tree;
@@ -36,27 +34,28 @@ public class Solution775 {
             this.tree = new int[n + 1];
         }
 
-        public void update(int num) {
-            for (int i = num; i < tree.length; i += lowbit(i)) {
-                tree[i] += 1;
-            }
+        public int queryNm(int index) {
+            return query(tree.length - 1) - query(index);
         }
 
-        public int queryNum(int num) {
-            return query(tree.length - 1) - query(num);
-        }
-
-        public int query(int num) {
+        public int query(int index) {
             int res = 0;
-            for (int i = num; i > 0; i -= lowbit(i)) {
+            for (int i = index; i > 0; i -= lowbit(i)) {
                 res += tree[i];
             }
 
             return res;
         }
 
+        public void update(int index) {
+            for (int i = index; i < tree.length; i += lowbit(i)) {
+                tree[i] += 1;
+            }
+        }
+
         private int lowbit(int i) {
             return i & -i;
         }
     }
+
 }
