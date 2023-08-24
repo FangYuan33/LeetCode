6b9fb2a50acddc1f9e343f23dd3e002cf76ea9c6
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-24 09:21:34 +0800

    1395. 统计作战单位数 review

--- src/leetcode/binaryindexedtree/Solution1395.java
+++ src/leetcode/binaryindexedtree/Solution1395.java
@@ -6,22 +6,26 @@ public class Solution1395 {
         System.out.println(new Solution1395().numTeams(new int[]{2, 5, 3, 4, 1}));
     }
 
+    // 本题的思路是固定 i 和 k 士兵，查询在 [i, k] 区间内有多少 j 士兵
+    // 区间查找和单点修改，符合线段树的用法
     public int numTeams(int[] rating) {
-        int res = 0;
-
         BinaryIndexedTree tree = new BinaryIndexedTree();
+
+        int res = 0;
         for (int i = 0; i < rating.length; i++) {
-            for (int j = i + 1; j < rating.length; j++) {
-                if (rating[j] > rating[i]) {
-                    res += tree.query(rating[i], rating[j]);
+            for (int k = i + 1; k < rating.length; k++) {
+                if (rating[i] < rating[k]) {
+                    res += tree.query(rating[i], rating[k]);
                 }
-                if (rating[j] < rating[i]) {
-                    res += tree.query(rating[j], rating[i]);
+                if (rating[i] > rating[k]) {
+                    res += tree.query(rating[k], rating[i]);
                 }
 
-                tree.update(rating[j], 1);
+                // 标记 rating[j] 士兵
+                tree.update(rating[k], 1);
             }
 
+            // 将 i + 1 标记过的士兵全部取消标记
             for (int j = i + 1; j < rating.length; j++) {
                 tree.update(rating[j], -1);
             }
@@ -31,20 +35,15 @@ public class Solution1395 {
     }
 
     static class BinaryIndexedTree {
+
         int[] tree;
 
         public BinaryIndexedTree() {
             this.tree = new int[(int) 1e5 + 1];
         }
 
-        public void update(int rating, int val) {
-            for (int i = rating; i < tree.length; i += lowbit(i)) {
-                tree[i] += val;
-            }
-        }
-
         public int query(int left, int right) {
-            return query(right) - query(left - 1);
+            return query(right) - query(left);
         }
 
         public int query(int num) {
@@ -54,6 +53,12 @@ public class Solution1395 {
             }
 
             return res;
+        }
+
+        public void update(int num, int val) {
+            for (int i = num; i < tree.length; i += lowbit(i)) {
+                tree[i] += val;
+            }
         }
 
         private int lowbit(int i) {
