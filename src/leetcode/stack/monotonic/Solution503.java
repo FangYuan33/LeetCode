a44c2506176337b23dd5ef40c42fc9a83f59204a
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2023-08-25 22:16:19 +0800

    503. 下一个更大元素 II review

--- src/leetcode/stack/monotonic/Solution503.java
+++ src/leetcode/stack/monotonic/Solution503.java
@@ -5,15 +5,16 @@ import java.util.Stack;
 
 public class Solution503 {
     public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
         int[] res = new int[nums.length];
         Arrays.fill(res, -1);
 
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < nums.length * 2; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % nums.length]) {
-                res[stack.pop()] = nums[i % nums.length];
+        for (int i = 0; i < n * 2; i++) {
+            while (!stack.isEmpty() && nums[i % n] > nums[stack.peek()]) {
+                res[stack.pop()] = nums[i % n];
             }
-            stack.push(i % nums.length);
+            stack.push(i % n);
         }
 
         return res;
