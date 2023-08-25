c627c93ce1f9e9bb38e3c2b7f0e8286d647b72bb
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-25 09:14:30 +0800

    396. 旋转函数 review

--- src/leetcode/prefixsum/Solution396.java
+++ src/leetcode/prefixsum/Solution396.java
@@ -8,21 +8,22 @@ public class Solution396 {
 
     public int maxRotateFunction(int[] nums) {
         int n = nums.length;
-        // 计算前缀和
         int[] sum = new int[n * 2];
+        // 计算 2 倍数组长度的前缀和
         sum[0] = nums[0];
         for (int i = 1; i < sum.length; i++) {
             sum[i] = sum[i - 1] + nums[i % n];
         }
 
-        // 计算第一组值
+        // 计算第一组结果
         int res = 0;
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             res += i * nums[i];
         }
 
+        // 从第一组不断的递推之后的
         int temp = res;
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             temp += (n - 1) * nums[i];
             temp -= sum[n - 1 + i] - sum[i];
 
