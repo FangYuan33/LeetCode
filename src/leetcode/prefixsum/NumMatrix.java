40a6367899a7dd251c66b1d1eb7860624023a21a
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-25 08:59:39 +0800

    304. 二维区域和检索 - 矩阵不可变 review

--- src/leetcode/prefixsum/NumMatrix.java
+++ src/leetcode/prefixsum/NumMatrix.java
@@ -12,7 +12,7 @@ public class NumMatrix {
 
     public NumMatrix(int[][] matrix) {
         sum = new int[matrix.length + 1][matrix[0].length + 1];
-
+        // 初始化二维数组前缀和
         for (int i = 1; i < sum.length; i++) {
             for (int j = 1; j < sum[0].length; j++) {
                 sum[i][j] = matrix[i - 1][j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
