964107423cc716af4d1caf3504204a4f662b6aa6
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-26 15:25:53 +0800

    327. 区间和的个数

--- README.md
+++ README.md
@@ -118,13 +118,13 @@
 
 - 树状数组解题模板：[BinaryIndexedTree.java](src%2Fleetcode%2Fbinaryindexedtree%2FBinaryIndexedTree.java)
 
-| 题目链接                                                                                                       | 题解                                                                          | 备注 |
-|------------------------------------------------------------------------------------------------------------|-----------------------------------------------------------------------------|----|
-| [307. 区域和检索 - 数组可修改 中等](https://leetcode.cn/problems/range-sum-query-mutable/)                             | [NumArray.java](src%2Fleetcode%2Fbinaryindexedtree%2FNumArray.java)         |    |
-| [1310. 子数组异或查询 中等](https://leetcode.cn/problems/xor-queries-of-a-subarray/)                                | [Solution1310.java](src%2Fleetcode%2Fbinaryindexedtree%2FSolution1310.java) |    |
-| [1395. 统计作战单位数 中等](https://leetcode.cn/problems/count-number-of-teams/)                                    | [Solution1395.java](src%2Fleetcode%2Fbinaryindexedtree%2FSolution1395.java) |    |
-| [775. 全局倒置与局部倒置 中等](https://leetcode.cn/problems/global-and-local-inversions/)                             | [Solution775.java](src%2Fleetcode%2Fbinaryindexedtree%2FSolution775.java)   |    |
-| [327. 区间和的个数 困难](https://leetcode.cn/problems/count-of-range-sum/)                                         |                                                                             |    |
+| 题目链接                                                                           | 题解                                                                          | 备注 |
+|--------------------------------------------------------------------------------|-----------------------------------------------------------------------------|----|
+| [307. 区域和检索 - 数组可修改 中等](https://leetcode.cn/problems/range-sum-query-mutable/) | [NumArray.java](src%2Fleetcode%2Fbinaryindexedtree%2FNumArray.java)         |    |
+| [1310. 子数组异或查询 中等](https://leetcode.cn/problems/xor-queries-of-a-subarray/)    | [Solution1310.java](src%2Fleetcode%2Fbinaryindexedtree%2FSolution1310.java) |    |
+| [1395. 统计作战单位数 中等](https://leetcode.cn/problems/count-number-of-teams/)        | [Solution1395.java](src%2Fleetcode%2Fbinaryindexedtree%2FSolution1395.java) |    |
+| [775. 全局倒置与局部倒置 中等](https://leetcode.cn/problems/global-and-local-inversions/) | [Solution775.java](src%2Fleetcode%2Fbinaryindexedtree%2FSolution775.java)   |    |
+| [327. 区间和的个数 困难](https://leetcode.cn/problems/count-of-range-sum/)             | [Solution327.java](src%2Fleetcode%2Fbinaryindexedtree%2FSolution327.java)   |    |
 
 ## 8. 线段树
 
--- src/leetcode/binaryindexedtree/Solution327.java
+++ src/leetcode/binaryindexedtree/Solution327.java
@@ -8,70 +8,78 @@ public class Solution327 {
         System.out.println(new Solution327().countRangeSum(new int[]{-2, 5, -1}, -2, 2));
     }
 
-    // 若我们处理到位置 k，以 k 为右端点的前缀和为 s，那么以 nums[k] 为右端点的合法子数组个数，
-    // 等价于在下标 [0, k - 1] 中前缀和范围在 [s - upper, s - lower] 的数的个数
-    //
     public int countRangeSum(int[] nums, int lower, int upper) {
-        // 前缀和计算
-        HashSet<Long> set = new HashSet<>();
+        // 先计算前缀和
         long preSum = 0L;
-        set.add(preSum);
+        Set<Long> allSum = new HashSet<>();
+        // 无元素的前缀和时为 0，需要添加进来
+        allSum.add(0L);
         for (int num : nums) {
             preSum += num;
 
-            set.add(preSum);
-            set.add(preSum - upper);
-            set.add(preSum - lower);
+            allSum.add(preSum);
+            // 区间范围也需要
+            allSum.add(preSum - upper);
+            allSum.add(preSum - lower);
         }
-        // 排序并离散化
-        ArrayList<Long> list = new ArrayList<>(set);
+
+        // 由于区间和值域爆炸，我们需要对它进行离散化处理
+        ArrayList<Long> list = new ArrayList<>(allSum);
         Collections.sort(list);
+        // 将对应的值映射为树状数组的索引上
         HashMap<Long, Integer> map = new HashMap<>();
         for (int i = 0; i < list.size(); i++) {
             map.put(list.get(i), i + 1);
         }
 
-        BinaryIndexedTree tree = new BinaryIndexedTree();
-        preSum = 0;
-        tree.add(map.get(preSum), 1);
         int res = 0;
+        BinaryIndexedTree tree = new BinaryIndexedTree();
+        // 标记上第一个元素前缀和前边的占位 0
+        preSum = 0L;
+        tree.update(map.get(preSum));
+
         for (int num : nums) {
             preSum += num;
-            Integer right = map.get(preSum - lower);
-            Integer left = map.get(preSum - upper);
 
+            // 由当前前缀和的后一个前缀和计算出区间范围
+            Integer left = map.get(preSum - upper);
+            Integer right = map.get(preSum - lower);
+            // 计算结果
             res += tree.query(left, right);
-            tree.add(map.get(preSum), 1);
+
+            // 更新上当前前缀和
+            tree.update(map.get(preSum));
         }
 
         return res;
     }
 
+    // 单点更新和区间查询
     static class BinaryIndexedTree {
 
         int[] tree;
 
         public BinaryIndexedTree() {
-            this.tree = new int[100010 * 3];
-        }
-
-        public void add(int index, int val) {
-            for (int i = index; i < tree.length; i += lowbit(i)) {
-                tree[i] += val;
-            }
+            tree = new int[(int) 3e5 + 1];
         }
 
         public int query(int left, int right) {
             return query(right) - query(left - 1);
         }
 
-        public int query(int index) {
+        public int query(int x) {
             int res = 0;
-            for (int i = index; i > 0; i -= lowbit(i)) {
+            for (int i = x; i > 0; i -= lowbit(i)) {
                 res += tree[i];
             }
 
             return res;
+        }
+
+        public void update(int index) {
+            for (int i = index; i < tree.length; i += lowbit(i)) {
+                tree[i] += 1;
+            }
         }
 
         private int lowbit(int i) {
