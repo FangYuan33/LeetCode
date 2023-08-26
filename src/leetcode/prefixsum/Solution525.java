45a53ad774cf90ad45f95cbbbcad0c35b643694f
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2023-08-26 23:33:13 +0800

    前缀和 review

--- src/leetcode/prefixsum/Solution525.java
+++ src/leetcode/prefixsum/Solution525.java
@@ -3,18 +3,22 @@ package leetcode.prefixsum;
 import java.util.HashMap;
 
 public class Solution525 {
+
+    public static void main(String[] args) {
+        new Solution525().findMaxLength(new int[]{0, 1, 0});
+    }
+
     public int findMaxLength(int[] nums) {
-        // 计算前缀和，并把 0 转换为 -1 来求解，这样符合题意的区间和为 0
+        // 计算前缀和
         int[] preSum = new int[nums.length + 1];
         for (int i = 1; i < preSum.length; i++) {
             preSum[i] = preSum[i - 1] + (nums[i - 1] == 0 ? -1 : 1);
         }
-        // 使用 map 来记录前缀和对应的最小的索引
+
+        int res = 0;
+        // 记录前缀和出现的最小索引值（只记录第一次）用于计算区间长度
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 0);
-
-        // 如果我们能在 map 中找到相同的前缀和，那么这个序列减去之前出现的序列即为 0 和 1 相等的连续子数组
-        int res = 0;
         for (int i = 1; i < preSum.length; i++) {
             if (map.containsKey(preSum[i])) {
                 res = Math.max(res, i - map.get(preSum[i]));
--- src/leetcode/prefixsum/Solution560.java
+++ src/leetcode/prefixsum/Solution560.java
@@ -8,17 +8,15 @@ public class Solution560 {
     }
 
     public int subarraySum(int[] nums, int k) {
-        // 记录对应的前缀和出现的次数
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
         int preSum = 0;
-        int res = 0;
+        // 记录对应前缀和的数量
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(preSum, 1);
 
+        int res = 0;
         for (int num : nums) {
             preSum += num;
-            // 检查先前有没有出现过这个前缀和（子序列）符合题目要求
             res += map.getOrDefault(preSum - k, 0);
-            // 记录前缀和出现的次数
             map.put(preSum, map.getOrDefault(preSum, 0) + 1);
         }
 
