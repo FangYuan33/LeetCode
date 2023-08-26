037894b223021da30be93dea3510bfd6abf475eb
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-26 19:24:41 +0800

    560. 和为 K 的子数组

--- README.md
+++ README.md
@@ -85,7 +85,7 @@
 
 ## 5. 前缀和
 
-连续子数组求和，不涉及元素值变化
+连续子数组求和，理解连续子数组很重要，**通过前缀和我们能计算出任意区间和**，利用这一点可以解决很多区间求和的问题，不过前缀和的计算不涉及元素值变化
 
 ### 一维数组前缀和
 
@@ -94,8 +94,8 @@
 | [303. 区域和检索 - 数组不可变 简单](https://leetcode.cn/problems/range-sum-query-immutable/) | [NumArray.java](src%2Fleetcode%2Fprefixsum%2FNumArray.java)       |                     |
 | [238. 除自身以外数组的乘积 中等](https://leetcode.cn/problems/product-of-array-except-self/) | [Solution238.java](src%2Fleetcode%2Fprefixsum%2FSolution238.java) |                     |
 | [396. 旋转函数 中等](https://leetcode.cn/problems/rotate-function/)                    | [Solution396.java](src%2Fleetcode%2Fprefixsum%2FSolution396.java) | 暴力法会直接超时，解决本题需要推导一下 |
+| [560. 和为 K 的子数组 中等](https://leetcode.cn/problems/subarray-sum-equals-k/)         | [Solution560.java](src%2Fleetcode%2Fprefixsum%2FSolution560.java) | ⭐️                  |
 
-1. [560. 和为 K 的子数组 中等](https://leetcode.cn/problems/subarray-sum-equals-k/)
 2. [525. 连续数组 中等](https://leetcode.cn/problems/contiguous-array/)
 
 ### 二维数组前缀和
--- src/leetcode/prefixsum/Solution560.java
+++ src/leetcode/prefixsum/Solution560.java
@@ -8,24 +8,18 @@ public class Solution560 {
     }
 
     public int subarraySum(int[] nums, int k) {
-        int res = 0;
-
-        // 计算前缀和
-        int[] preSum = new int[nums.length + 1];
-        for (int i = 0; i < nums.length; i++) {
-            preSum[i + 1] = preSum[i] + nums[i];
-        }
-
-        // 统计前缀和出现的次数
+        // 记录对应的前缀和出现的次数
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
+        int preSum = 0;
+        int res = 0;
 
-        for (int i = 1; i < preSum.length; i++) {
-            // 检查这个前缀和有没有在所有前缀和中出现过，如果有累加次数
-            int sum = preSum[i] - k;
-            res += map.getOrDefault(sum, 0);
-            // 把这个前缀和统计进去
-            map.put(preSum[i], map.getOrDefault(preSum[i], 0) + 1);
+        for (int num : nums) {
+            preSum += num;
+            // 检查先前有没有出现过这个前缀和（子序列）符合题目要求
+            res += map.getOrDefault(preSum - k, 0);
+            // 记录前缀和出现的次数
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
         }
 
         return res;
