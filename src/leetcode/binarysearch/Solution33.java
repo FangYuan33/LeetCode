4f9c0012ce8ebc812597d4f5430c51b673f0b3cf
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-08-31 11:26:32 +0800

    33. 搜索旋转排序数组 review

--- src/leetcode/binarysearch/Solution33.java
+++ src/leetcode/binarysearch/Solution33.java
@@ -8,23 +8,17 @@ public class Solution33 {
 
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
+
         while (left <= right) {
             int mid = left + right >> 1;
 
-            // 说明到了右边
-            if (nums[mid] > nums[right]) {
-                if (target < nums[left]) {
-                    // 比右边最小值还小 那么必须去左边找了
-                    left = mid + 1;
-                    continue;
-                }
-            } else {
-                // 到了左边
-                if (target > nums[right]) {
-                    // 比左边最大值还大，那么得去右边找了
-                    right = mid - 1;
-                    continue;
-                }
+            if (nums[mid] > nums[right] && target < nums[left]) {
+                left = mid + 1;
+                continue;
+            }
+            if (nums[mid] < nums[right] && target > nums[right]) {
+                right = mid - 1;
+                continue;
             }
 
             if (target > nums[mid]) {
