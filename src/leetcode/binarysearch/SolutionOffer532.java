9203cc978c45590dee6d3905cd3ab0d00dcf14db
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-09-01 18:01:05 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

---
+++ src/leetcode/binarysearch/SolutionOffer532.java
@@ -0,0 +1,24 @@
+package leetcode.binarysearch;
+
+public class SolutionOffer532 {
+
+    public static void main(String[] args) {
+        System.out.println(new SolutionOffer532().missingNumber(new int[]{0, 2, 3}));
+    }
+
+    public int missingNumber(int[] nums) {
+        int left = 0, right = nums.length;
+
+        while (left < right) {
+            int mid = left + right >> 1;
+
+            if (nums[mid] == mid) {
+                left = mid + 1;
+            } else if (mid < nums[mid]) {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
