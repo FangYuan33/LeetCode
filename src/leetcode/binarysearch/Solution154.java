cecf38e95b7a89f7508db1d95301b7a7c84f7100
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-09-03 21:56:07 +0800

    154. 寻找旋转排序数组中的最小值 II

--- src/leetcode/binarysearch/Solution154.java
+++ src/leetcode/binarysearch/Solution154.java
@@ -7,34 +7,34 @@ public class Solution154 {
 //        int[] nums = new int[]{1, 3, 5};
 //        int[] nums = new int[]{2, 2, 2, 0, 1};
         int[] nums = new int[]{3, 3, 1, 3};
+//        int[] nums = new int[]{3, 1, 1};
 
         System.out.println(new Solution154().findMin(nums));
     }
 
     public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + right >> 1;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] < nums[right]) {
-                // 落在了小区间
-                right = mid;
-            } else if (nums[mid] > nums[right]) {
-                // 落在了大区间
+            // 一定落在了大区间
+            if (nums[mid] > nums[right]) {
                 left = mid + 1;
-            } else {
-                // 相等情况，那么需要线性查找，而且存在左右两个区间都是相等的值的情况
-                // 它可能在mid的左右两侧，答案一定在[left, right]之间
-                int index = left;
-                for (int i = left + 1; i < right; i++) {
-                    if (nums[i] < nums[index]) {
-                        index = i;
-                    }
-                }
-
-                return nums[index];
+                continue;
             }
+            // 一定落在了小区间
+            if (nums[mid] < nums[right]) {
+                right = mid;
+                continue;
+            }
+
+            // 相等的情况可能在大区间也可能在小区间，不过现在的答案一定在 [left, right] 区间内，所以遍历找一下即可
+            int min = nums[left];
+            for (int i = left + 1; i <= right; i++) {
+                min = Math.min(min, nums[i]);
+            }
+
+            return min;
         }
 
         return nums[left];
