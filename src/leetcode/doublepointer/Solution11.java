a00ffd2b4cb7f9c6a3b13595dba301bf566e1027
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-09-04 20:21:56 +0800

    11. 盛最多水的容器

--- src/leetcode/doublepointer/Solution11.java
+++ src/leetcode/doublepointer/Solution11.java
@@ -3,19 +3,16 @@ package leetcode.doublepointer;
 public class Solution11 {
     public int maxArea(int[] height) {
         int left = 0, right = height.length - 1;
+
         int res = 0;
-
         while (left < right) {
-            int x = right - left;
-            int y = Math.min(height[left], height[right]);
-            res = Math.max(res, x * y);
-
-            // 小的高度变
-            if (height[left] < height[right]) {
-                left++;
+            int h;
+            if (height[left] <= height[right]) {
+                h = height[left++];
             } else {
-                right--;
+                h = height[right--];
             }
+            res = Math.max(res, h * (right - left + 1));
         }
 
         return res;
