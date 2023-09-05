7e44799686151ad3954b71501e355e8092288904
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-09-05 15:15:31 +0800

    26. 删除有序数组中的重复项

---
+++ src/leetcode/doublepointer/Solution26.java
@@ -0,0 +1,17 @@
+package leetcode.doublepointer;
+
+public class Solution26 {
+    public int removeDuplicates(int[] nums) {
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            while (right + 1 < nums.length && nums[right + 1] == nums[right]) {
+                right++;
+            }
+            if (right < nums.length) {
+                nums[left++] = nums[right++];
+            }
+        }
+
+        return left + 1;
+    }
+}
