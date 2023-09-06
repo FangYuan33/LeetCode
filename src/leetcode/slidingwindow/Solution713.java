6ec2bf2fd035aa6fde38c4e9e705f29abe9f0e9c
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-09-06 23:01:38 +0800

    713. 乘积小于 K 的子数组

--- src/leetcode/slidingwindow/Solution713.java
+++ src/leetcode/slidingwindow/Solution713.java
@@ -1,34 +1,31 @@
 package leetcode.slidingwindow;
 
 public class Solution713 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution713().numSubarrayProductLessThanK(new int[]{1, 1, 1}, 2));
+    }
+
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         if (k == 0 || k == 1) {
             return 0;
         }
 
         int res = 0;
-        // 确定窗口左值开始滑动，之后不断地切换窗口大小
-        for (int left = 0; left < nums.length; left++) {
-            int sum = 1;
-            int right = left;
+        int sum = 1;
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            sum *= nums[right];
 
-            while (right < nums.length) {
-                sum *= nums[right];
-                if (sum < k) {
-                    res++;
-                    // 窗口后滑变大
-                    right++;
-                } else {
-                    // 窗口值超过指定值
-                    break;
-                }
+            while (sum >= k) {
+                sum /= nums[left];
             }
+
+            res += right - left + 1;
+            right++;
         }
 
         return res;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Solution713().numSubarrayProductLessThanK(new int[]{1, 1, 1}, 2));
-    }
 }
