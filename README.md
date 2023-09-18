e9049ed9cdd2aa1b460cb7449a28998ca026dd57
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-09-18 20:29:32 +0800

    524. 通过删除字母匹配到字典里最长单词

--- README.md
+++ README.md
@@ -32,6 +32,7 @@
 | [413. 等差数列划分 中等](https://leetcode.cn/problems/arithmetic-slices/)                                                                  | [Solution413.java](src%2Fleetcode%2Fslidingwindow%2FSolution413.java)         |                                          |
 | [443. 压缩字符串 中等](https://leetcode.cn/problems/string-compression/)                                                                  | [Solution443.java](src%2Fleetcode%2Fdoublepointer%2FSolution443.java)         |                                          |
 | [481. 神奇字符串 中等](https://leetcode.cn/problems/magical-string/)                                                                      | [Solution481.java](src%2Fleetcode%2Fdoublepointer%2FSolution481.java)         |                                          |
+| [524. 通过删除字母匹配到字典里最长单词 中等](https://leetcode.cn/problems/longest-word-in-dictionary-through-deleting/)                              | [Solution524.java](src%2Fleetcode%2Fdoublepointer%2FSolution524.java)         |                                          |
 
 ## 滑动窗口
 
---
+++ src/leetcode/doublepointer/Solution524.java
@@ -0,0 +1,39 @@
+package leetcode.doublepointer;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solution524 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution524().findLongestWord("abpcplea", Arrays.asList("ale", "apple", "monkey", "plea")));
+    }
+
+    public String findLongestWord(String s, List<String> dictionary) {
+        dictionary.sort((x, y) -> {
+            if (x.length() != y.length()) {
+                return y.length() - x.length();
+            }
+            return x.compareTo(y);
+        });
+
+        String res = "";
+        for (String d : dictionary) {
+            if (d.length() > res.length()) {
+                int sIndex = 0, dIndex = 0;
+                while (dIndex < d.length() && sIndex < s.length()) {
+                    if (d.charAt(dIndex) == s.charAt(sIndex)) {
+                        dIndex++;
+                    }
+                    sIndex++;
+                }
+                if (dIndex == d.length()) {
+                    res = d;
+                }
+            }
+        }
+
+        return res;
+    }
+}
