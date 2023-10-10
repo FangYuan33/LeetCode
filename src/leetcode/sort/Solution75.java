dc7c135092bc74dcb2b7e5bc114f302a93b098e7
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-10-10 15:54:56 +0800

    75. 颜色分类

--- src/leetcode/sort/Solution75.java
+++ src/leetcode/sort/Solution75.java
@@ -2,25 +2,47 @@ package leetcode.sort;
 
 public class Solution75 {
     public void sortColors(int[] nums) {
-        int zeroIndex = 0;
-        int twoIndex = nums.length - 1;
-        int begin = 0;
+        sort(nums, 0, nums.length - 1);
+    }
 
-        while (begin <= twoIndex) {
-            // 除了2挪到合适的位置不需要begin++外
-            // 剩下的不过是0和1之间的交换而已
-            if (nums[begin] == 0) {
-                swap(nums, begin, zeroIndex);
-                zeroIndex++;
-                begin++;
-            } else if (nums[begin] == 2) {
-                // 换到2的时候没有执行 begin++
-                // 因为被换过来的这个值可能还需要被交换位置
-                swap(nums, begin, twoIndex);
-                twoIndex--;
-            } else {
-                begin++;
+    private void sort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        betterBase(nums, left, right);
+
+        int partition = partition(nums, left, right);
+
+        sort(nums, left, partition - 1);
+        sort(nums, partition + 1,  right);
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int originIndex = left;
+        int base = nums[left];
+
+        while (left < right) {
+            while (left < right && nums[right] >= base) {
+                right--;
             }
+            while (left < right && nums[left] <= base) {
+                left++;
+            }
+            swap(nums, left, right);
+        }
+        swap(nums, originIndex, left);
+
+        return left;
+    }
+
+    private void betterBase(int[] nums, int left, int right) {
+        int mid = left + right >> 1;
+
+        if ((nums[mid] < nums[right]) ^ (nums[mid] < nums[left])) {
+            swap(nums, left, mid);
+        } else if ((nums[right] < nums[mid]) ^ (nums[right] < nums[left])) {
+            swap(nums, left, right);
         }
     }
 
