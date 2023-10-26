49cf6213cc4b6450e99d359ee4583cb3c0fb58be
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-10-26 20:49:53 +0800

    114. 二叉树展开为链表

---
+++ src/leetcode/tree/recursion/Solution114.java
@@ -0,0 +1,27 @@
+package leetcode.tree.recursion;
+
+import leetcode.TreeNode;
+
+public class Solution114 {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left != null) {
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+            TreeNode rightPre = left;
+            while (rightPre.right != null) {
+                rightPre = rightPre.right;
+            }
+            rightPre.right = right;
+            root.right = left;
+            root.left = null;
+        }
+
+        flatten(root.right);
+    }
+
+
+}
