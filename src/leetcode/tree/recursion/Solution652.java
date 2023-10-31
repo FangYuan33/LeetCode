e3f1a8b79eb66b313c0aee08b62315e2198091c8
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-10-31 15:33:04 +0800

    652. 寻找重复的子树

---
+++ src/leetcode/tree/recursion/Solution652.java
@@ -0,0 +1,40 @@
+package leetcode.tree.recursion;
+
+import leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution652 {
+
+    List<TreeNode> res;
+
+    HashMap<String, Integer> map;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        res = new ArrayList<>();
+        map = new HashMap<>();
+        recursion(root);
+
+        return res;
+    }
+
+    private String recursion(TreeNode root) {
+        if (root == null) {
+            return " ";
+        }
+
+        StringBuilder nodes = new StringBuilder();
+        nodes.append(root.val).append("_");
+        nodes.append(recursion(root.left)).append(recursion(root.right));
+
+        String key = nodes.toString();
+        map.put(key, map.getOrDefault(key, 0) + 1);
+        if (map.get(key) == 2) {
+            res.add(root);
+        }
+
+        return key;
+    }
+}
