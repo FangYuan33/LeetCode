aa6a6d7a550219db0088e2c0cdc87cd16f3ae05f
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-11-25 21:06:46 +0800

    1395. 统计作战单位数

--- src/leetcode/binaryindexedtree/Solution1395.java
+++ src/leetcode/binaryindexedtree/Solution1395.java
@@ -7,22 +7,23 @@ public class Solution1395 {
     }
 
     public int numTeams(int[] rating) {
-        int res = 0;
+        int[] left = new int[rating.length];
         BinaryIndexedTree tree = new BinaryIndexedTree();
         for (int i = 0; i < rating.length; i++) {
-            for (int j = i + 1; j < rating.length; j++) {
-                if (rating[i] < rating[j]) {
-                    res += tree.queryResult(rating[i], rating[j]);
-                }
-                if (rating[i] > rating[j]) {
-                    res += tree.queryResult(rating[j], rating[i]);
-                }
-                tree.update(rating[j], 1);
-            }
+            left[i] = tree.query(rating[i]);
+            tree.update(rating[i], 1);
+        }
+        int[] right = new int[rating.length];
+        BinaryIndexedTree tree2 = new BinaryIndexedTree();
+        for (int i = rating.length - 1; i >= 0; i--) {
+            right[i] = tree2.queryResult(rating[i], (int) 1e5);
+            tree2.update(rating[i], 1);
+        }
 
-            for (int j = i + 1; j < rating.length; j++) {
-                tree.update(rating[j], - 1);
-            }
+        int res = 0;
+        for (int i = 0; i < rating.length; i++) {
+            res += left[i] * right[i];
+            res += (i - left[i]) * (rating.length - i - 1 - right[i]);
         }
 
         return res;
