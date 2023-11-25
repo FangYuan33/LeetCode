a9d5b7e0aa3117c2755cba18f83680f86cef0323
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-11-25 17:34:09 +0800

    线段树 review

--- README.md
+++ README.md
@@ -247,7 +247,7 @@
 | [729. 我的日程安排表 I 中等](https://leetcode.cn/problems/my-calendar-i/)     | [MyCalendar.java](src%2Fleetcode%2Fsegmenttree%2FMyCalendar.java)           |                                |
 | [731. 我的日程安排表 II 中等](https://leetcode.cn/problems/my-calendar-ii/)   | [MyCalendarTwo.java](src%2Fleetcode%2Fsegmenttree%2FMyCalendarTwo.java)     | ⭐️                             |
 | [732. 我的日程安排表 III 困难](https://leetcode.cn/problems/my-calendar-iii/) | [MyCalendarThree.java](src%2Fleetcode%2Fsegmenttree%2FMyCalendarThree.java) |                                |
-| [715. Range 模块 困难](https://leetcode.cn/problems/range-module/)       | [RangeModule.java](src%2Fleetcode%2Fsegmenttree%2FRangeModule.java)         | ⭐️ 理解 add 是作为区间变化量还是区间内单个值的变化量 |
+| [715. Range 模块 困难](https://leetcode.cn/problems/range-module/)       | [RangeModule.java](src%2Fleetcode%2Fsegmenttree%2FRangeModule.java)         | ✅️ 理解 add 是作为区间变化量还是区间内单个值的变化量 |
 
 ## 字符串
 
--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -9,36 +9,5 @@ import java.util.*;
  * @since 2023-11-14 09:26:23
  */
 public class Solution {
-    public int[] getOrder(int[][] tasks) {
-        HashMap<int[], Integer> taskNum = new HashMap<>();
-        for (int i = 0; i < tasks.length; i++) {
-            taskNum.put(tasks[i], i);
-        }
-        Arrays.sort(tasks, Comparator.comparingInt(x -> x[0]));
-        int index = 0;
-        int[] res = new int[tasks.length];
 
-        int i = 0, currentTime = tasks[0][0];
-        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((x, y) -> {
-            if (x[1] == y[1]) {
-                return taskNum.get(x) - taskNum.get(y);
-            } else {
-                return x[1] - y[1];
-            }
-        });
-        while (i < tasks.length || !priorityQueue.isEmpty()) {
-            while (i < tasks.length && currentTime >= tasks[i][0]) {
-                priorityQueue.offer(tasks[i++]);
-            }
-            if (!priorityQueue.isEmpty()) {
-                int[] cur = priorityQueue.poll();
-                res[index++] = taskNum.get(cur);
-                currentTime += cur[1];
-            } else if (i < tasks.length) {
-                currentTime = tasks[i][0];
-            }
-        }
-
-        return res;
-    }
 }
