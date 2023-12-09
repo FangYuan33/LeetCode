90177db3dc3ecde9cc8f99688ffa0eb43478efd4
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-12-09 13:59:13 +0800

    fix

---
+++ src/leetcode/hashmap/FreqStack.java
@@ -0,0 +1,15 @@
+package leetcode.hashmap;
+
+public class FreqStack {
+//    public FreqStack() {
+//
+//    }
+//
+//    public void push(int val) {
+//
+//    }
+//
+//    public int pop() {
+//
+//    }
+}
