9f470412d20fc262f9338784827735181caf0df1
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2023-12-16 15:45:57 +0800

    895. 最大频率栈

--- src/leetcode/hashmap/FreqStack.java
+++ src/leetcode/hashmap/FreqStack.java
@@ -1,15 +1,41 @@
 package leetcode.hashmap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class FreqStack {
-//    public FreqStack() {
-//
-//    }
-//
-//    public void push(int val) {
-//
-//    }
-//
-//    public int pop() {
-//
-//    }
+
+    HashMap<Integer, Integer> numFreq;
+
+    HashMap<Integer, List<Integer>> freqNum;
+
+    int maxFreq;
+
+    public FreqStack() {
+        numFreq = new HashMap<>();
+        freqNum = new HashMap<>();
+        maxFreq = 0;
+    }
+
+    public void push(int val) {
+        int freq = numFreq.getOrDefault(val, 0) + 1;
+        numFreq.put(val, freq);
+
+        maxFreq = Math.max(maxFreq, freq);
+
+        List<Integer> list = freqNum.getOrDefault(freq, new ArrayList<>());
+        list.add(val);
+        freqNum.put(freq, list);
+    }
+
+    public int pop() {
+        List<Integer> list = freqNum.get(maxFreq);
+        Integer res = list.remove(list.size() - 1);
+        numFreq.put(res, maxFreq - 1);
+        if (list.size() == 0) {
+            maxFreq--;
+        }
+        return res;
+    }
 }
