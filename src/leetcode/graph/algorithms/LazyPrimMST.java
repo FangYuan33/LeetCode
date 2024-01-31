2abd8a1a2e72ba39da734f2b97081eb837910da2
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2024-01-31 20:23:01 +0800

    图：最小生成树的Prim算法

---
+++ src/leetcode/graph/algorithms/LazyPrimMST.java
@@ -0,0 +1,53 @@
+package leetcode.graph.algorithms;
+
+import leetcode.graph.algorithms.basic.Edge;
+import leetcode.graph.algorithms.basic.EdgeWeightedGraph;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 获取加权无向图中最小生成树的Prim算法-延迟移除失效的边版
+ *
+ * @author FangYuan
+ * @since 2024-01-31 13:21:53
+ */
+public class LazyPrimMST {
+
+    private boolean[] marked;
+
+    private Queue<Edge> mst;
+
+    private PriorityQueue<Edge> pq;
+
+    public LazyPrimMST(EdgeWeightedGraph digraph) {
+        pq = new PriorityQueue<>();
+        marked = new boolean[digraph.getV()];
+        mst = new PriorityQueue<>();
+
+        while (!pq.isEmpty()) {
+            Edge min = pq.poll();
+
+            int e = min.either(), o = min.other(e);
+            if (marked[e] && marked[o]) {
+                continue;
+            }
+            mst.offer(min);
+            if (!marked[e]) {
+                visit(digraph, e);
+            }
+            if (!marked[o]) {
+                visit(digraph, o);
+            }
+        }
+    }
+
+    private void visit(EdgeWeightedGraph digraph, int v) {
+        marked[v] = true;
+        for (Edge w : digraph.adj(v)) {
+            if (!marked[w.other(v)]) {
+                pq.offer(w);
+            }
+        }
+    }
+}
