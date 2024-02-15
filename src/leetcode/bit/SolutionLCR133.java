bb66c7c00041b45f3893d2c36688202fb6b1dd4e
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2024-02-15 18:27:37 +0800

    LCR 133. 位 1 的个数

--- src/leetcode/bit/SolutionLCR133.java
+++ src/leetcode/bit/SolutionLCR133.java
@@ -1,14 +1,15 @@
 package leetcode.bit;
 
-public class SolutionOffer15 {
+public class SolutionLCR133 {
+    // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int res = 0;
-
         while (n != 0) {
-            res += n & 1;
+            if ((n & 1) == 1) {
+                res++;
+            }
             n >>>= 1;
         }
-
         return res;
     }
 }
