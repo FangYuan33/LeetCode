f537049085e76a3ae1a4efd36bd833e2ab00873e
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2024-02-15 18:59:30 +0800

    137. 只出现一次的数字 II

--- src/leetcode/bit/Solution137.java
+++ src/leetcode/bit/Solution137.java
@@ -3,18 +3,17 @@ package leetcode.bit;
 public class Solution137 {
     public int singleNumber(int[] nums) {
         int[] bits = new int[32];
-
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 31; j >= 0; j--) {
+            for (int j = bits.length - 1; j >= 0; j--) {
                 bits[j] += nums[i] & 1;
                 nums[i] >>>= 1;
             }
         }
 
         int res = 0;
-        for (int i = 0; i < bits.length; i++) {
+        for (int bit : bits) {
             res <<= 1;
-            res |= bits[i] % 3;
+            res |= bit % 3;
         }
 
         return res;
