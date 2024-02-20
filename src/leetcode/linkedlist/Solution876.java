b1ed4113ee4d8442ee279fe1616397f4e489a541
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2024-02-20 09:28:46 +0800

    876. 链表的中间结点

---
+++ src/leetcode/linkedlist/Solution876.java
@@ -0,0 +1,16 @@
+package leetcode.linkedlist;
+
+import leetcode.ListNode;
+
+public class Solution876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
