9c9272a9a435e27fba0bc6af9adbce74ebab2e58
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2024-03-07 09:28:43 +0800

    191. 位1的个数

---
+++ src/leetcode/bit/Solution191.java
@@ -0,0 +1,14 @@
+package leetcode.bit;
+
+public class Solution191 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += n & 1;
+            n >>>= 1;
+        }
+
+        return res;
+    }
+}
