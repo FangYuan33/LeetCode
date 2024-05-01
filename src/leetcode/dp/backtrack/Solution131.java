967fa1f758b8a37db56cefd983f8db6520db3500
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2024-05-01 15:10:27 +0800

    131. 分割回文串

--- src/leetcode/dp/backtrack/Solution131.java
+++ src/leetcode/dp/backtrack/Solution131.java
@@ -1,6 +1,6 @@
 package leetcode.dp.backtrack;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution131 {
@@ -9,34 +9,39 @@ public class Solution131 {
         System.out.println(new Solution131().partition("aab"));
     }
 
+    // 定义全局变量
     List<List<String>> res;
 
     public List<List<String>> partition(String s) {
-        res = new ArrayList<>();
-        backtrack(s, new ArrayList<>(), 0);
+        res = new LinkedList<>();
+        backtrack(s, 0, new LinkedList<>());
         return res;
     }
 
-    private void backtrack(String s, ArrayList<String> element, int begin) {
+    private void backtrack(String s, int begin, LinkedList<String> path) {
+        // 所有子串均为回文串，添加答案并结束
         if (begin == s.length()) {
-            res.add((List<String>) element.clone());
+            res.add((List<String>) path.clone());
             return;
         }
 
         for (int i = begin; i < s.length(); i++) {
-            if (isReverse(s.substring(begin, i + 1))) {
-                element.add(s.substring(begin, i + 1));
-                backtrack(s, element, i + 1);
+            String cur = s.substring(begin, i + 1);
+            // 每一步的操作：判断是否为回文串，是的话加入路径中，并继续处理子问题
+            if (isReverse(cur)) {
+                path.add(cur);
+                // 子问题：从大于小于等于下标 i + 1 的子数组中判断并构造回文串集合
+                backtrack(s, i + 1, path);
                 // 恢复现场
-                element.removeLast();
+                path.removeLast();
             }
         }
     }
 
-    private boolean isReverse(String origin) {
-        int left = 0, right = origin.length() - 1;
+    private boolean isReverse(String s) {
+        int left = 0, right = s.length() - 1;
         while (left < right) {
-            if (origin.charAt(left) != origin.charAt(right)) {
+            if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
             left++;
