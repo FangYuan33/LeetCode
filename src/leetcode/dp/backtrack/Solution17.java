925d1f99fd48138457237b57f75c18c2d1ea04e3
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2024-05-01 10:30:00 +0800

    17. 电话号码的字母组合

---
+++ src/leetcode/dp/backtrack/Solution17.java
@@ -0,0 +1,37 @@
+package leetcode.dp.backtrack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution17 {
+
+    String[] numChars = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    List<String> res;
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        res = new ArrayList<>();
+        backtrack(digits, 0, new StringBuilder());
+        return res;
+    }
+
+    private void backtrack(String digits, int index, StringBuilder element) {
+        if (index == digits.length()) {
+            res.add(element.toString());
+            return;
+        }
+
+        char[] charArray = numChars[digits.charAt(index) - '0'].toCharArray();
+        for (char c : charArray) {
+            element.append(c);
+            backtrack(digits, index + 1, element);
+            // 恢复现场
+            element.deleteCharAt(element.length() - 1);
+        }
+    }
+}
