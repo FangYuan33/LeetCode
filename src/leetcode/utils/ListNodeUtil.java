3642f2f8d8de152e9f4d2a6e0552de4cb53d26aa
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2026-02-11 14:26:22 +0800

    hot_100

---
+++ src/leetcode/utils/ListNodeUtil.java
@@ -0,0 +1,26 @@
+package leetcode.utils;
+
+import leetcode.ListNode;
+
+public class ListNodeUtil {
+
+    // eg: [1,2,2,1]
+    public static ListNode generate(int[] listNodes) {
+        if (listNodes == null || listNodes.length == 0) {
+            return null;
+        }
+        
+        // 创建头节点
+        ListNode head = new ListNode(listNodes[0]);
+        ListNode current = head;
+        
+        // 依次创建后续节点
+        for (int i = 1; i < listNodes.length; i++) {
+            current.next = new ListNode(listNodes[i]);
+            current = current.next;
+        }
+        
+        return head;
+    }
+
+}
