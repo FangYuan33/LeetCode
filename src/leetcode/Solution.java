3654ff14da11ee2f38d5ee4b7a6ec2f41580972f
FangYuan33 <61506177+FangYuan33@users.noreply.github.com>
2026-03-22 14:51:11 +0800

    hot 100

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -1,5 +1,11 @@
 package leetcode;
 
+import leetcode.utils.Matrix;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 供本地题解使用
  *
@@ -7,5 +13,26 @@ package leetcode;
  * @since 2023-11-14 09:26:23
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18});
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        int res = 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(nums, 1);
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
 
 }
